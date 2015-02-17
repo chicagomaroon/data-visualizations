8cec701ce338725fa3cb9e702744a1e4b9859361
Chicago Maroon <forrest@wireless-s0-so-150-40-36.uchicago.edu>
2015-02-17 01:15:23 -0600

    3

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
  
     return false;
   });
+    
     $(window).on('scroll', function () {
         if ($(this).scrollTop() > 550 && !$('.nav').hasClass('visible')) {
             $('.nav').animate({
@@ -297,3 +298,8 @@ $(document).ready(function () {
     });
                  
 });
+                 function fbShare(url, title, descr, image, winWidth, winHeight) {
+        var winTop = (screen.height / 2) - (winHeight / 2);
+        var winLeft = (screen.width / 2) - (winWidth / 2);
+        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+    }
