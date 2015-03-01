81831a4c1a1075126753053929fdc7faa5c745bb
Chicago Maroon <forrest@wireless-s0-so-150-43-100.uchicago.edu>
2015-03-01 17:44:23 -0600

    Poll updated

--- aramark/main.js
+++ aramark/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    $('.take-poll-btn').click(function(event) {
+        $(this).toggle();
+        $('#hidden-iframe,form,.poll-border').toggle();
+    });
+    if($.cookie('v') == 'true')
+        voted();
     $('#fb-icon').click(function (event) {
         event.preventDefault();
         FB.ui({
@@ -23,5 +29,79 @@ $(document).ready(function () {
 
         return false;
     });
+    $('#ss-submit').click(function (event) {
+        voted();
+    });
+     $('#chart-1').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Aramark Historical Performance'
+        },
+        xAxis: {
+            categories: ['2012', '2013', '2014'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'USD (thousands)',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true,
+                    formatter: function () {
+                        return "$" + this.y.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    }
+                }
+            }
+        },
+        legend: {
+            layout: 'horizontal',
+            align: 'right',
+            verticalAlign: 'bottom',
+            floating: false,
+            borderWidth: 0,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: false
+        },
+        credits: {
+            enabled: true,
+            text: 'Source: U.S. Securities and Exchange Commission',
+            href: '',
+        },
+        series: [{
+            name: 'Revenue',
+            data: [13505426, 13945657, 14832913],
+            animation: false
+        }, {
+            name: 'Operating Income/EBIT',
+            data: [581775, 514474, 564563],
+            animation: false
+        }, {
+            name: 'Net Income',
+            data: [107199, 70366, 149459],
+            animation: false
+        }],
+        
+    });
 
 });
+function voted() {
+    $('#hidden-iframe,form,.take-poll-btn').remove();
+    $('.alert-success,.alert-title,.poll-border').toggle();
+    if($.cookie('v') !== 'true')
+        $.cookie('v','true',{expires: 365});
+}
