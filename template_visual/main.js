aa6a9fa046b5357096834f5a4539b06d72485925
michplunkett <michplunkett@gmail.com>
2023-02-21 18:23:45 -0600

    template updates

--- template_visual/main.js
+++ template_visual/main.js
@@ -1,5 +1,13 @@
-import * as am5 from '@amcharts/amcharts5';
-
 // For API and chart documentation please look here:
 // https://www.amcharts.com/demos/
-const root = am5.Root.new('chart-div');
+am5.ready(function () {
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new('chart-div');
+
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    // All code for your chart goes here
+});
