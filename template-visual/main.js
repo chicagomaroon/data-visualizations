c147a2a38d5d42ba8bbb4271693d8686daee53c5
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-23 22:40:20 -0500

    Add default color template (#22)
    ## What does this PR do?
    Added default color scheme.
    
    ## How was it tested?
    - [x] Used a default chart and added the colors to it.
    <img width="1431" alt="Screenshot 2023-04-23 at 9 56 21 PM"
    src="https://user-images.githubusercontent.com/5885605/233894232-d8c5a54f-7a12-4a41-8113-b8ad5bf9032d.png">

--- template-visual/main.js
+++ template-visual/main.js
@@ -5,9 +5,22 @@ am5.ready(function () {
     // https://www.amcharts.com/docs/v5/getting-started/#Root_element
     var root = am5.Root.new('chart-div');
 
-    // Set themes
-    // https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([am5themes_Animated.new(root)]);
+    // Setting themes and default colors
+    var maroonTheme = am5.Theme.new(root);
+    maroonTheme
+        .rule('ColorSet')
+        .set('colors', [
+            am5.color('#C76363'),
+            am5.color('#C04A49'),
+            am5.color('#A42323'),
+            am5.color('#7F1416'),
+            am5.color('#571612'),
+            am5.color('#3D3D3D'),
+            am5.color('#392F83'),
+            am5.color('#184F26')
+        ]);
+
+    root.setThemes([am5themes_Animated.new(root), maroonTheme]);
 
     // All code for your chart goes here
 });
