ba58e7adc8d161adbb3c8feec8f0080e3be5f189
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-03 23:59:18 -0500

    Update main.js

--- template-visual/main.js
+++ template-visual/main.js
@@ -1,40 +1,35 @@
 // For API and chart documentation please look here:
 // https://www.highcharts.com/demo
-am5.ready(function () {
-    // Setting themes and default colors
-    Highcharts.theme = {
-        colors: [
-            '#C76363',
-            '#C04A49',
-            '#A42323',
-            '#7F1416',
-            '#571612',
-            '#3D3D3D',
-            '#392F83',
-            '#184F26'
-        ]
-    };
-    Highcharts.setOptions(Highcharts.theme);
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#C76363',
+        '#C04A49',
+        '#A42323',
+        '#7F1416',
+        '#571612',
+        '#3D3D3D',
+        '#392F83',
+        '#184F26'
+    ],
 
     // All code for your chart goes here
-    Highcharts.chart('chart-div', {
-        title: {
-            text: '[TITLE]',
-            align: 'center'
-        },
+    title: {
+        text: '[TITLE]',
+        align: 'center'
+    },
 
-        plotOptions: {
-            series: {
-                // PUT STARTING SPACE FOR YOUR DATA
-                pointStart: 2010
-            }
-        },
+    plotOptions: {
+        series: {
+            // PUT STARTING SPACE FOR YOUR DATA
+            pointStart: 2010
+        }
+    },
 
-        series: [
-            {
-                name: '[DATA FIELD NAME]',
-                data: []
-            }
-        ]
-    });
+    series: [
+        {
+            name: '[DATA FIELD NAME]',
+            data: []
+        }
+    ]
 });
