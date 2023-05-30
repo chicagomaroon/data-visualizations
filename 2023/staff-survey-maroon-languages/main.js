9fff32413c1c886c50c0177c583bf402f728b9f2
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-30 16:08:32 -0500

    Finished language visual

---
+++ 2023/staff-survey-maroon-languages/main.js
@@ -0,0 +1,97 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+const data = [
+    {
+        name: 'Hindi',
+        weight: 2
+    },
+    {
+        name: 'Japanese',
+        weight: 1
+    },
+    {
+        name: 'German',
+        weight: 3
+    },
+    {
+        name: 'Hungarian',
+        weight: 1
+    },
+    {
+        name: 'Swahili',
+        weight: 1
+    },
+    {
+        name: 'English',
+        weight: 37
+    },
+    {
+        name: 'Bengali',
+        weight: 1
+    },
+    {
+        name: 'Spanish',
+        weight: 9
+    },
+    {
+        name: 'Mandarin',
+        weight: 5
+    },
+    {
+        name: 'French',
+        weight: 3
+    },
+    {
+        name: 'Korean',
+        weight: 2
+    },
+    {
+        name: 'Italian',
+        weight: 1
+    },
+    {
+        name: 'Cantonese',
+        weight: 1
+    },
+    {
+        name: 'Greek',
+        weight: 1
+    }
+];
+
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#C76363',
+        '#C04A49',
+        '#A42323',
+        '#7F1416',
+        '#571612',
+        '#3D3D3D',
+        '#392F83',
+        '#184F26'
+    ],
+
+    // All code for your chart goes here
+    accessibility: {
+        screenReaderSection: {
+            beforeChartFormat:
+                '<h5>{chartTitle}</h5>' + '<div>{viewTableButton}</div>'
+        }
+    },
+    series: [
+        {
+            type: 'wordcloud',
+            data,
+            name: 'Occurrences'
+        }
+    ],
+    title: {
+        text: 'The Maroon Staff Survey 2023: Spoken Languages',
+        align: 'center'
+    },
+    tooltip: {
+        headerFormat:
+            '<span style="font-size: 16px"><b>{point.key}</b></span><br>'
+    }
+});
