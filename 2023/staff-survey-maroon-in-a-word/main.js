dc026626225e126229030723b1bf16cd2aff00b6
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-31 17:44:35 -0500

    Update main.js

--- 2023/staff-survey-maroon-in-a-word/main.js
+++ 2023/staff-survey-maroon-in-a-word/main.js
@@ -1,3 +1,114 @@
+const descriptions = [
+    {
+        name: 'Dedicated',
+        weight: 2
+    },
+    {
+        name: 'Responsible',
+        weight: 1
+    },
+    {
+        name: 'Reform',
+        weight: 1
+    },
+    {
+        name: 'Fluid',
+        weight: 1
+    },
+    {
+        name: 'Inquisitive',
+        weight: 1
+    },
+    {
+        name: 'Cautious',
+        weight: 1
+    },
+    {
+        name: 'Welcoming',
+        weight: 3
+    },
+    {
+        name: 'Funk',
+        weight: 1
+    },
+    {
+        name: 'Inviting',
+        weight: 1
+    },
+    {
+        name: 'Valuable',
+        weight: 1
+    },
+    {
+        name: 'Curiosity',
+        weight: 1
+    },
+    {
+        name: 'Hard-Working',
+        weight: 1
+    },
+    {
+        name: 'Committed',
+        weight: 1
+    },
+    {
+        name: 'Driven',
+        weight: 1
+    },
+    {
+        name: 'Collaborative',
+        weight: 2
+    },
+    {
+        name: 'Exciting',
+        weight: 1
+    },
+    {
+        name: 'Independent',
+        weight: 1
+    },
+    {
+        name: 'Informative',
+        weight: 1
+    },
+    {
+        name: 'Learning',
+        weight: 1
+    },
+    {
+        name: 'Everything',
+        weight: 1
+    },
+    {
+        name: 'Close',
+        weight: 1
+    },
+    {
+        name: 'Busy',
+        weight: 1
+    },
+    {
+        name: 'Knowledgeable',
+        weight: 1
+    },
+    {
+        name: 'Large',
+        weight: 1
+    },
+    {
+        name: 'Chill',
+        weight: 1
+    },
+    {
+        name: 'Unparalleled',
+        weight: 1
+    },
+    {
+        name: 'Meowparty',
+        weight: 1
+    }
+];
+
 // For API and chart documentation please look here:
 // https://www.highcharts.com/demo
 Highcharts.chart('chart-div', {
@@ -14,24 +125,26 @@ Highcharts.chart('chart-div', {
     ],
 
     // All code for your chart goes here
-    title: {
-        text: '[TITLE]',
-        align: 'center'
-    },
-
-    plotOptions: {
-        series: {
-            // PUT PLOT CONFIG OPTIONS HERE SPACE FOR YOUR DATA
+    // All code for your chart goes here
+    accessibility: {
+        screenReaderSection: {
+            beforeChartFormat:
+                '<h5>{chartTitle}</h5>' + '<div>{viewTableButton}</div>'
         }
     },
-
     series: [
         {
-            name: '[DATA FIELD NAME]',
-            label: {
-                enabled: false
-            },
-            data: []
+            type: 'wordcloud',
+            data: descriptions,
+            name: 'Occurrences'
         }
-    ]
+    ],
+    title: {
+        text: 'The Maroon Staff Survey 2023: Maroon-in-a-Word',
+        align: 'center'
+    },
+    tooltip: {
+        headerFormat:
+            '<span style="font-size: 16px"><b>{point.key}</b></span><br>'
+    }
 });
