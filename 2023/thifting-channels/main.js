6188e617e5e990e283b2ecc25906f6712f2342fe
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-31 21:48:10 -0500

    Channels

--- 2023/thifting-channels/main.js
+++ 2023/thifting-channels/main.js
@@ -3,35 +3,84 @@
 Highcharts.chart('chart-div', {
     // Setting default colors
     colors: [
-        '#C76363',
-        '#C04A49',
-        '#A42323',
-        '#7F1416',
-        '#571612',
-        '#3D3D3D',
-        '#392F83',
-        '#184F26'
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
     ],
 
     // All code for your chart goes here
-    title: {
-        text: '[TITLE]',
-        align: 'center'
+    chart: {
+        type: 'column'
     },
-
-    plotOptions: {
-        series: {
-            // PUT PLOT CONFIG OPTIONS HERE SPACE FOR YOUR DATA
+    title: {
+        text: 'Global Market Sizing and Growth Estimates for 2023'
+    },
+    xAxis: {
+        categories: ['2012', '2022', '2032']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Market Share'
         }
     },
-
+    tooltip: {
+        pointFormat:
+            '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            stacking: 'percent'
+        }
+    },
     series: [
         {
-            name: '[DATA FIELD NAME]',
-            label: {
-                enabled: false
-            },
-            data: []
+            name: 'Off Price',
+            data: [11, 16, 19]
+        },
+        {
+            name: 'Secondhand',
+            data: [3, 9, 18]
+        },
+        {
+            name: 'Mid-Priced Specialty',
+            data: [25, 16, 12]
+        },
+        {
+            name: 'Direct to Consumer',
+            data: [4, 11, 12]
+        },
+        {
+            name: 'Value Chains',
+            data: [12, 14, 10]
+        },
+        {
+            name: 'Fast Fashion',
+            data: [8, 9, 9]
+        },
+        {
+            name: 'Other Retailers',
+            data: [15, 11, 9]
+        },
+        {
+            name: 'Department Stores',
+            data: [20, 10, 5]
+        },
+        {
+            name: 'Amazon Fashion',
+            data: [1, 3, 5]
+        },
+        {
+            name: 'Rental and Subscription',
+            data: [1, 1, 1]
         }
     ]
 });
