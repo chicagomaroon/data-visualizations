7107a51725929879ef5a94044576b066af9c62a1
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-31 21:25:22 -0500

    Thrifting growth

--- 2023/thifting-growth/main.js
+++ 2023/thifting-growth/main.js
@@ -1,37 +1,128 @@
+const defaultColors = [
+    '#800000',
+    '#FFA319',
+    '#C16622',
+    '#8F3931',
+    '#8A9045',
+    '#58593F',
+    '#155F83',
+    '#350E20'
+];
+
 // For API and chart documentation please look here:
 // https://www.highcharts.com/demo
-Highcharts.chart('chart-div', {
+Highcharts.chart('chart-cohort', {
     // Setting default colors
-    colors: [
-        '#C76363',
-        '#C04A49',
-        '#A42323',
-        '#7F1416',
-        '#571612',
-        '#3D3D3D',
-        '#392F83',
-        '#184F26'
-    ],
+    colors: defaultColors,
 
     // All code for your chart goes here
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
     title: {
-        text: '[TITLE]',
+        text: 'Secondhand Growth by Shopping Cohort through 2027',
         align: 'center'
     },
-
-    plotOptions: {
-        series: {
-            // PUT PLOT CONFIG OPTIONS HERE SPACE FOR YOUR DATA
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
         }
     },
-
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
     series: [
         {
-            name: '[DATA FIELD NAME]',
-            label: {
-                enabled: false
-            },
-            data: []
+            name: 'Shoppers',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'New',
+                    y: 60.0,
+                    selected: true
+                },
+                {
+                    name: 'Existing',
+                    y: 40.0
+                }
+            ]
+        }
+    ]
+});
+
+Highcharts.chart('chart-generation', {
+    // Setting default colors
+    colors: defaultColors,
+
+    // All code for your chart goes here
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Secondhand Growth by Generation through 2027',
+        align: 'center'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [
+        {
+            name: 'Generation',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Millennial',
+                    y: 33,
+                    selected: true
+                },
+                {
+                    name: 'Gen X',
+                    y: 26
+                },
+                {
+                    name: 'Gen Z',
+                    y: 28
+                },
+                {
+                    name: 'Gen Alpha',
+                    y: 1
+                },
+                {
+                    name: 'Baby Boomer',
+                    y: 12
+                }
+            ]
         }
     ]
 });
