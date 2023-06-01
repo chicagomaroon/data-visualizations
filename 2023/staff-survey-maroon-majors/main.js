7bb37792e1a5cefeb0efccfae879eb2d48f8fef6
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-31 20:33:32 -0500

    Putting majors data in place

--- 2023/staff-survey-maroon-majors/main.js
+++ 2023/staff-survey-maroon-majors/main.js
@@ -1,3 +1,142 @@
+const majorsData = [
+    {
+        major: 'Romance Language and Literature',
+        weight: 1
+    },
+    {
+        major: 'Astrophysics',
+        weight: 1
+    },
+    {
+        major: 'Law Letters and Society',
+        weight: 4
+    },
+    {
+        major: 'Critical Race and Ethnic Studies',
+        weight: 1
+    },
+    {
+        major: 'Public Policy',
+        weight: 4
+    },
+    {
+        major: 'Economics',
+        weight: 12
+    },
+    {
+        major: 'Undecided',
+        weight: 1
+    },
+    {
+        major: 'Music',
+        weight: 1
+    },
+    {
+        major: 'Comparative Literature',
+        weight: 1
+    },
+    {
+        major: 'Spanish',
+        weight: 1
+    },
+    {
+        major: 'Biological Sciences',
+        weight: 1
+    },
+    {
+        major: 'Biochemistry',
+        weight: 1
+    },
+    {
+        major: 'Cinema and Media Studies',
+        weight: 1
+    },
+    {
+        major: 'Physics',
+        weight: 1
+    },
+    {
+        major: 'Linguistics',
+        weight: 1
+    },
+    {
+        major: 'Mathematics',
+        weight: 1
+    },
+    {
+        major: 'Environmental Science',
+        weight: 2
+    },
+    {
+        major: 'Fundamentals',
+        weight: 1
+    },
+    {
+        major: 'Urban Studies',
+        weight: 1
+    },
+    {
+        major: 'Psychology',
+        weight: 1
+    },
+    {
+        major: 'English and Creative Writing',
+        weight: 2
+    },
+    {
+        major: 'Global Studies',
+        weight: 3
+    },
+    {
+        major: 'Statistics',
+        weight: 1
+    },
+    {
+        major: 'English',
+        weight: 6
+    },
+    {
+        major: 'English Literature and Language',
+        weight: 1
+    },
+    {
+        major: 'Sociology',
+        weight: 4
+    },
+    {
+        major: 'Data Science',
+        weight: 1
+    },
+    {
+        major: 'Human Rights',
+        weight: 2
+    },
+    {
+        major: 'Political Science',
+        weight: 4
+    },
+    {
+        major: 'Visual Art',
+        weight: 1
+    },
+    {
+        major: 'Computer Science',
+        weight: 4
+    },
+    {
+        major: 'History',
+        weight: 5
+    },
+    {
+        major: 'Russian and East European Studies',
+        weight: 1
+    },
+    {
+        major: 'Chemistry',
+        weight: 1
+    }
+];
+
 // For API and chart documentation please look here:
 // https://www.highcharts.com/demo
 Highcharts.chart('chart-div', {
@@ -15,7 +154,7 @@ Highcharts.chart('chart-div', {
 
     // All code for your chart goes here
     title: {
-        text: '[TITLE]',
+        text: 'The Maroon Staff Survey 2023: Represented Majors',
         align: 'center'
     },
 
