ef965dbd2c36b792af72abda12c50b9634149eac
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-01 22:49:22 -0500

    Change colors back for word blobs

--- 2023/staff-survey-maroon-in-a-word/main.js
+++ 2023/staff-survey-maroon-in-a-word/main.js
@@ -3,16 +3,14 @@
 Highcharts.chart('chart-div', {
     // Setting default colors
     colors: [
-        '#800000',
-        '#FFA319',
-        '#C16622',
-        '#8F3931',
-        '#8A9045',
-        '#58593F',
-        '#155F83',
-        '#350E20',
-        '#47B5FF',
-        '#FF3399'
+        '#C76363',
+        '#C04A49',
+        '#A42323',
+        '#7F1416',
+        '#571612',
+        '#3D3D3D',
+        '#392F83',
+        '#184F26'
     ],
 
     // All code for your chart goes here
