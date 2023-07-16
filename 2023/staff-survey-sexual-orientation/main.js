29422613307d4151a2ddc3ac3a2381263193d187
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-16 17:47:53 -0500

    Update main.js

--- 2023/staff-survey-sexual-orientation/main.js
+++ 2023/staff-survey-sexual-orientation/main.js
@@ -2,38 +2,80 @@
 // https://www.highcharts.com/demo
 Highcharts.chart('chart-div', {
     // Setting default colors
-    colors: [
-        '#800000',
-        '#FFA319',
-        '#C16622',
-        '#8F3931',
-        '#8A9045',
-        '#58593F',
-        '#155F83',
-        '#350E20',
-        '#47B5FF',
-        '#FF3399'
-    ],
+    colors: ['#800000', '#FD4C4C', '#FC0000', '#7D2525', '#C90000'],
 
     // All code for your chart goes here
+    chart: {
+        type: 'column'
+    },
+
     title: {
-        text: '[TITLE]',
+        text: 'The Maroon Staff Survey 2023: Sexual Orientation',
         align: 'center'
     },
 
-    plotOptions: {
-        series: {
-            // PUT PLOT CONFIG OPTIONS HERE SPACE FOR YOUR DATA
+    legend: {
+        enabled: false
+    },
+
+    xAxis: {
+        accessibility: {
+            description: 'Sexual Orientation'
+        },
+        type: 'category'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Members in Category'
         }
+    },
+
+    tooltip: {
+        headerFormat:
+            '<span style="font-size:11px;font-weight:bold;">Sexual Orientation:</span><br>',
+        pointFormat:
+            '<span style="display: flex; justify-content: center;">{point.y} members identify as {point.name}</span>'
     },
 
     series: [
         {
-            name: '[DATA FIELD NAME]',
-            label: {
-                enabled: false
-            },
-            data: []
+            name: 'Sexual Orientation',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Aromantic/Asexual',
+                    y: 9
+                },
+                {
+                    name: 'Bisexual',
+                    y: 14
+                },
+                {
+                    name: 'Gay',
+                    y: 2
+                },
+                {
+                    name: 'Lesbian',
+                    y: 1
+                },
+                {
+                    name: 'Queer',
+                    y: 5
+                },
+                {
+                    name: 'Questioning Or Unsure',
+                    y: 3
+                },
+                {
+                    name: 'Straight (Heterosexual)',
+                    y: 24
+                },
+                {
+                    name: 'Prefer Not To Disclose',
+                    y: 1
+                }
+            ]
         }
     ]
 });
