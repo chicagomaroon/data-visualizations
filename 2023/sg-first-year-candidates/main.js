6fb88058aa2adea5c3534f91d2de03d411af6d15
Nik4002 <pateln4002@gmail.com>
2023-10-29 12:59:57 -0500

    Created plot in folder "/2023/sg-first-year-candidates"

---
+++ 2023/sg-first-year-candidates/main.js
@@ -0,0 +1,77 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    // All code for your chart goes here
+    title: {
+        text: 'First-Year Candidates for College Council by Year',
+        align: 'left'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Candidates'
+        },
+        max: 20, 
+        min: 0
+    },
+
+    xAxis: {
+        accessibility: {
+            rangeDescription: 'Range: 2020 to 2023'
+        },
+        tickPositioner: function () {
+            return [2020, 2021, 2022, 2023];
+        }
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2020
+        }
+    },
+
+    series: [{
+        data: [19, 18, 15, 9],
+        name: "Number of candidates",
+        label: {enabled: false},
+        showInLegend: false
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+});
