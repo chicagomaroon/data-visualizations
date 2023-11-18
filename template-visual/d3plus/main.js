0caee2eea64803c4d1dcd771418c01d0a5a19f56
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-18 00:24:33 -0600

    Create main.js

---
+++ template-visual/d3plus/main.js
@@ -0,0 +1,4 @@
+// For API and chart documentation please look here:
+// https://d3plus.org/?path=/docs/introduction--documentation
+// The color palette is following list:
+// ['#800000', '#FFA319', '#C16622', '#8F3931', '#8A9045', '#58593F', '#155F83', '#350E20', '#47B5FF', '#FF3399']
