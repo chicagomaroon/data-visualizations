46738f40f03643505cb69d93ae4dd36d58fbe592
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-23 21:23:28 -0600

    Add tenure-track visuals (#49)
    ## What does this PR do?
    Adds tenure-track info visuals.
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [x] This visual was approved by the lead developer.
    - [x] The visual was approved by the story's stakeholders.
    
    <img width="1470" alt="Screenshot 2023-12-23 at 8 35 38 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/5885605/e55b3a6f-5273-4f31-9a3b-1c5012e8d924">
    
    <img width="1470" alt="Screenshot 2023-12-23 at 8 35 46 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/5885605/f44ad28d-eb48-4a5d-b934-78fa44e9291d">
    
    <img width="1064" alt="Screenshot 2023-12-23 at 8 49 32 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/5885605/3cd42a79-f2d7-442f-8a55-02952d524325">
    
    <img width="1065" alt="Screenshot 2023-12-23 at 8 49 38 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/5885605/5dff8cb8-baf8-4e59-b18e-6f40597b755a">

---
+++ 2023/professor-teaching/main.js
@@ -0,0 +1,55 @@
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: ['#800000', '#FFA319'],
+
+    title: {
+        text: 'Undergraduates Taught by Type of Instructor',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'For the the Academic Year of 2020 ',
+        align: 'center'
+    },
+    chart: {
+        type: 'column'
+    },
+    xAxis: {
+        categories: [
+            'Tenure and Tenure-Track Faculty',
+            'Union Lecturers',
+            'Lecturers',
+            'Students',
+            'Col. Asst. Professors/Instructors',
+            'Other'
+        ],
+        accessibility: {
+            description: 'Instructor Types'
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Percentage of Undergraduates (%)'
+        }
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+
+    series: [
+        {
+            name: 'Undergraduate Student Population',
+            label: {
+                enabled: false
+            },
+            data: [44, 23, 12, 9, 7, 4]
+        },
+        {
+            name: 'Undergraduate Classes',
+            label: {
+                enabled: false
+            },
+            data: [38, 27, 11, 12, 7, 6]
+        }
+    ]
+});
---
+++ 2023/tenure-track-by-fiscal-year/main.js
@@ -0,0 +1,80 @@
+Highcharts.chart('chart-div', {
+    colors: ['#800000'],
+
+    title: {
+        text: 'Tenure and Tenure-Track Undergraduate Teaching',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'Fiscal Years of 2006 through 2020',
+        align: 'center'
+    },
+
+    xAxis: {
+        categories: [
+            '2006',
+            '2007*',
+            '2008*',
+            '2009',
+            '2010*',
+            '2011*',
+            '2012',
+            '2013*',
+            '2014*',
+            '2015',
+            '2016*',
+            '2017',
+            '2018',
+            '2019',
+            '2020'
+        ],
+        accessibility: {
+            rangeDescription: 'Fiscal Year Range: 2006 to 2020'
+        },
+        title: {
+            text: '* Years are intermediate years in range imputed from known 3-year interval data points'
+        }
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    yAxis: {
+        min: 30,
+        title: {
+            text: 'Percent of Total Undergraduate Courses'
+        }
+    },
+
+    tooltip: {
+        valueSuffix: '%'
+    },
+
+    series: [
+        {
+            name: 'Classes Taught',
+            connectNulls: true,
+            label: {
+                enabled: false
+            },
+            data: [
+                45,
+                null,
+                null,
+                41,
+                null,
+                null,
+                39,
+                null,
+                null,
+                40,
+                null,
+                42,
+                38,
+                37,
+                38
+            ]
+        }
+    ]
+});
