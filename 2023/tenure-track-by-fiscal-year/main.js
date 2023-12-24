8265e8d53da3ede2709e8e8f183f2a01b838e50a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-23 20:50:52 -0600

    Minify HTML

--- 2023/tenure-track-by-fiscal-year/main.js
+++ 2023/tenure-track-by-fiscal-year/main.js
@@ -11,12 +11,28 @@ Highcharts.chart('chart-div', {
     },
 
     xAxis: {
-        categories: [2006, 2009, 2012, 2015, 2017, 2018, 2019, 2020],
+        categories: [
+            '2006',
+            '2007*',
+            '2008*',
+            '2009',
+            '2010*',
+            '2011*',
+            '2012',
+            '2013*',
+            '2014*',
+            '2015',
+            '2016*',
+            '2017',
+            '2018',
+            '2019',
+            '2020'
+        ],
         accessibility: {
             rangeDescription: 'Fiscal Year Range: 2006 to 2020'
         },
         title: {
-            text: '*Excluded years are intermediate years in range imputed from known 3-year interval data points'
+            text: '* Years are intermediate years in range imputed from known 3-year interval data points'
         }
     },
 
@@ -38,10 +54,27 @@ Highcharts.chart('chart-div', {
     series: [
         {
             name: 'Classes Taught',
+            connectNulls: true,
             label: {
                 enabled: false
             },
-            data: [45, 41, 39, 40, 42, 38, 37, 38]
+            data: [
+                45,
+                null,
+                null,
+                41,
+                null,
+                null,
+                39,
+                null,
+                null,
+                40,
+                null,
+                42,
+                38,
+                37,
+                38
+            ]
         }
     ]
 });
