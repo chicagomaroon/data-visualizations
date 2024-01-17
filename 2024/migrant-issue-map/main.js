37167f10355f262e531c1471e5fd3130a6f154fd
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-01-17 13:56:37 -0600

    first commit

---
+++ 2024/migrant-issue-map/main.js
@@ -0,0 +1,396 @@
+const DATA = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            id: '0',
+            type: 'Feature',
+            properties: {
+                name: 'AIC',
+                description: '<strong>AIC</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6464061, 41.9550208, 0.0]
+            }
+        },
+        {
+            id: '1',
+            type: 'Feature',
+            properties: {
+                name: 'Brands Park',
+                description: '<strong>Brands Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6992049, 41.9411802, 0.0]
+            }
+        },
+        {
+            id: '2',
+            type: 'Feature',
+            properties: {
+                name: 'Broadway Armory Park',
+                description: '<strong>Broadway Armory Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6598587, 41.9891929, 0.0]
+            }
+        },
+        {
+            id: '3',
+            type: 'Feature',
+            properties: {
+                name: 'Casa Esperanza',
+                description: '<strong>Casa Esperanza</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7151664, 41.8659924, 0.0]
+            }
+        },
+        {
+            id: '4',
+            type: 'Feature',
+            properties: {
+                name: 'Chicago Lake Shore Hotel',
+                description: '<strong>Chicago Lake Shore Hotel</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.5856401, 41.8059408, 0.0]
+            }
+        },
+        {
+            id: '5',
+            type: 'Feature',
+            properties: {
+                name: 'Daley College',
+                description: '<strong>Daley College</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7228798, 41.7570501, 0.0]
+            }
+        },
+        {
+            id: '6',
+            type: 'Feature',
+            properties: {
+                name: 'Gage Park',
+                description: '<strong>Gage Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7035777, 41.7935203, 0.0]
+            }
+        },
+        {
+            id: '7',
+            type: 'Feature',
+            properties: {
+                name: 'Halsted Shelter',
+                description: '<strong>Halsted Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6457439, 41.851402, 0.0]
+            }
+        },
+        {
+            id: '8',
+            type: 'Feature',
+            properties: {
+                name: 'Inn of Chicago',
+                description: '<strong>Inn of Chicago</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6230694, 41.8928452, 0.0]
+            }
+        },
+        {
+            id: '9',
+            type: 'Feature',
+            properties: {
+                name: 'Leone Beach Park',
+                description: '<strong>Leone Beach Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.662313, 42.0132045, 0.0]
+            }
+        },
+        {
+            id: '10',
+            type: 'Feature',
+            properties: {
+                name: 'MWRD',
+                description: '<strong>MWRD</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7058692, 41.9764379, 0.0]
+            }
+        },
+        {
+            id: '11',
+            type: 'Feature',
+            properties: {
+                name: 'Near South',
+                description: '<strong>Near South</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.62288, 41.8302083, 0.0]
+            }
+        },
+        {
+            id: '12',
+            type: 'Feature',
+            properties: {
+                name: 'New Life Community Church Lakeview',
+                description:
+                    '<strong>New Life Community Church Lakeview</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6787227, 41.9358319, 0.0]
+            }
+        },
+        {
+            id: '13',
+            type: 'Feature',
+            properties: {
+                name: 'North Park Village',
+                description: '<strong>North Park Village</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7238491, 41.9838682, 0.0]
+            }
+        },
+        {
+            id: '14',
+            type: 'Feature',
+            properties: {
+                name: 'Ogden Shelter',
+                description: '<strong>Ogden Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6619782, 41.8877935, 0.0]
+            }
+        },
+        {
+            id: '15',
+            type: 'Feature',
+            properties: {
+                name: 'Parthenon Guest House (Hostel)',
+                description: '<strong>Parthenon Guest House (Hostel)</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6474612, 41.8775112, 0.0]
+            }
+        },
+        {
+            id: '16',
+            type: 'Feature',
+            properties: {
+                name: 'Piotrowski (Lillian) Park',
+                description: '<strong>Piotrowski (Lillian) Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7309295, 41.8348342, 0.0]
+            }
+        },
+        {
+            id: '17',
+            type: 'Feature',
+            properties: {
+                name: 'Social Club',
+                description: '<strong>Social Club</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.629022, 41.877706, 0.0]
+            }
+        },
+        {
+            id: '18',
+            type: 'Feature',
+            properties: {
+                name: 'Super 8',
+                description: '<strong>Super 8</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6646751, 42.0142016, 0.0]
+            }
+        },
+        {
+            id: '19',
+            type: 'Feature',
+            properties: {
+                name: 'Wadsworth',
+                description: '<strong>Wadsworth</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.5980831, 41.7784533, 0.0]
+            }
+        },
+        {
+            id: '20',
+            type: 'Feature',
+            properties: {
+                name: 'Walnut Shelter',
+                description: '<strong>Walnut Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6686713, 41.8863555, 0.0]
+            }
+        },
+        {
+            id: '21',
+            type: 'Feature',
+            properties: {
+                name: 'West Lake Street Shelter',
+                description: '<strong>West Lake Street Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.651114, 41.8854717, 0.0]
+            }
+        },
+        {
+            id: '22',
+            type: 'Feature',
+            properties: {
+                name: 'North Western Shelter',
+                description: '<strong>North Western Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.687129, 41.8916781, 0.0]
+            }
+        },
+        {
+            id: '23',
+            type: 'Feature',
+            properties: {
+                name: 'YMCA High Ridge',
+                description: '<strong>YMCA High Ridge</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6914618, 42.0125928, 0.0]
+            }
+        },
+        {
+            id: '24',
+            type: 'Feature',
+            properties: {
+                name: "Young Women's Leadership Academy",
+                description:
+                    "<strong>Young Women's Leadership Academy</strong>",
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6184451, 41.8444336, 0.0]
+            }
+        }
+    ]
+};
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiYXN0ZWluaGFydCIsImEiOiJjbHAxY3k5dTUwZ3lyMmxvaTJwendscThoIn0.0CTzLgJkNSInFlIrmfjZzQ';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-87.62077199094739, 41.9],
+    zoom: 10.5
+});
+
+map.on('load', () => {
+    map.addSource('places', {
+        // This GeoJSON contains features that include an "icon"
+        // property. The value of the "icon" property corresponds
+        // to an image in the Mapbox Streets style's sprite.
+        type: 'geojson',
+        data: DATA
+    });
+
+    // Add a layer showing the places.
+    map.addLayer({
+        id: 'places',
+        type: 'symbol',
+        source: 'places',
+        layout: {
+            'icon-image': ['get', 'icon'],
+            'icon-allow-overlap': true,
+            'icon-size': 2
+        }
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'places', (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+    });
+});
