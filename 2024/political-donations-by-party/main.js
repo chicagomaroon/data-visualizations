642fe541e2127ed723fd31d6f2eb8e6fd42a0e33
Austin Steinhart <45241656+Asteinhart@users.noreply.github.com>
2024-02-05 18:59:54 -0500

    Add political donation visuals (#50)
    ## What does this PR do?
    v3 of visuals for a story about UChicago political donations over the
    last 4 cycles. Draft in slack.
    * Also added a jupyter notebook in a new folder `data-analysis` folder
    (`fec_donations.ipynb`) which has the data work for all 3 charts as well
    as data sets in `raw_data/`
    * Not minified as a still draft commit. 
    * moved to a new 2024 folder.
    * updated chart titles, and axis for compare chart (from great
    suggestions from @tiffanyl9 !)
    * added additional chart for top packs. Made two (bubble and bar) but I
    like the bar chart better currently
    
    <img width="632" alt="Screenshot 2024-02-01 at 10 18 27 AM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/45241656/5a2c99cf-4520-4930-aa0c-cacf86220582">
    
    <img width="1506" alt="Screenshot 2024-02-05 at 3 12 09 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/45241656/aaa0a9a0-2a34-4ce9-8bc0-08dc30cc721c">
    
    <img width="1501" alt="Screenshot 2024-02-05 at 3 11 19 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/45241656/82001c11-5ae9-4d52-a0d1-95fdcee914c4">
    
    <img width="1493" alt="Screenshot 2024-02-05 at 3 10 33 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/45241656/5b0b6953-7229-453f-a2ae-6b13da548357">
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [x] This visual was approved by the lead developer.
    - [x] The visual was approved by the story's stakeholders.
    
    <!-- Replace this comment with a screenshot of your visual. -->
    
    ---------
    
    Co-authored-by: Michael Plunkett <5885605+michplunkett@users.noreply.github.com>

---
+++ 2024/political-donations-by-party/main.js
@@ -0,0 +1,146 @@
+var drawGraph = function () {
+    //variables for the font family, and some colors
+    var fontFamily = 'helvetica';
+    var demSquare = '#155F83';
+    var repSquare = '#800000';
+    var svgBackgroundColor = '#ffffff';
+
+    //width and height of the SVG
+    const width = 900;
+    const height = 500;
+
+    //create an svg with width and height
+    var svg = d3
+        .select('#grid-chart')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background-color', svgBackgroundColor);
+
+    //dem_data
+    var demNumRows = 12;
+    var demNumCols = 20;
+
+    //rep_data
+    var repNumRows = 2;
+    var repNumCols = 4;
+
+    //x and y axis scales
+    var y = d3.scaleBand().range([0, 200]).domain(d3.range(demNumRows));
+
+    var x = d3.scaleBand().range([0, 360]).domain(d3.range(demNumCols));
+
+    //the data is just an array of numbers for each cell in the grid
+    var demData = d3.range(demNumRows * demNumCols);
+
+    var repData = d3.range(repNumRows * repNumCols);
+
+    const demTranslate = 'translate(' + width * 0.5 + ',' + height * 0.3 + ')';
+
+    //container to hold the grid
+    var demContainer = svg.append('g').attr('transform', demTranslate);
+
+    demContainer
+        .selectAll('square')
+        .data(demData)
+        .enter()
+        .append('rect')
+        .attr('class', 'square')
+        .attr('id', function (d) {
+            return 'id' + d;
+        })
+        .attr('x', function (d) {
+            return x(d % demNumCols);
+        })
+        .attr('y', function (d) {
+            return y(Math.floor(d / demNumCols));
+        })
+        .attr('height', 13)
+        .attr('width', 13)
+        .attr('fill', demSquare)
+        .style('stroke', demSquare);
+
+    const repTranslate =
+        'translate(' + width * 0.3 + ',' + height * 0.6413 + ')';
+
+    var repContainer = svg.append('g').attr('transform', repTranslate);
+
+    repContainer
+        .selectAll('square')
+        .data(repData)
+        .enter()
+        .append('rect')
+        .attr('class', 'square')
+        .attr('id', function (d) {
+            return 'id' + d;
+        })
+        .attr('x', function (d) {
+            return x(d % repNumCols);
+        })
+        .attr('y', function (d) {
+            return y(Math.floor(d / repNumCols));
+        })
+        .attr('height', 13)
+        .attr('width', 13)
+        .attr('fill', repSquare)
+        .style('stroke', repSquare);
+
+    var title = svg
+        .append('text')
+        .attr('class', 'title')
+        .attr('x', 175)
+        .attr('y', 100)
+        .attr('text-anchor', 'start')
+        .attr('font-weight', 700)
+        .attr('font-size', 20)
+        .text(
+            'Between 2015 and 2023, University of Chicago faculty have donated...'
+        );
+
+    var title = svg
+        .append('text')
+        .attr('class', 'rep-text')
+        .attr('x', 305)
+        .attr('y', 380)
+        .attr('font-weight', 700)
+        .attr('text-anchor', 'middle')
+        .style('fill', repSquare)
+        .text('~$80,000')
+        .append('tspan')
+        .attr('font-weight', 500)
+        .style('fill', 'black')
+        .text(' to Republican');
+
+    var title = svg
+        .append('text')
+        .attr('class', 'rep-text')
+        .attr('x', 305)
+        .attr('y', 400)
+        .attr('text-anchor', 'middle')
+        .text('candidates and PACs');
+
+    var title = svg
+        .append('text')
+        .attr('class', 'rep-text')
+        .attr('x', 630)
+        .attr('y', 380)
+        .attr('text-anchor', 'middle')
+        .attr('font-weight', 700)
+        .style('fill', demSquare)
+        .text('~$2,400,000')
+        .append('tspan')
+        .attr('font-weight', 500)
+        .style('fill', 'black')
+        .text(' to Democratic');
+
+    var title = svg
+        .append('text')
+        .attr('class', 'rep-text')
+        .attr('x', 630)
+        .attr('y', 400)
+        .attr('text-anchor', 'middle')
+        .text('candidates and PACs');
+};
+
+//call function to draw the graph
+drawGraph();
