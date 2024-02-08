45acd41427016f4f925d7e8b2c45adfb775bad79
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-07 23:13:28 -0600

    Update main.js

--- 2024/political-donations-by-party/main.js
+++ 2024/political-donations-by-party/main.js
@@ -1,16 +1,15 @@
 var drawGraph = function () {
     //variables for the font family, and some colors
-    var fontFamily = 'helvetica';
-    var demSquare = '#155F83';
-    var repSquare = '#800000';
-    var svgBackgroundColor = '#ffffff';
+    const demSquare = '#155F83';
+    const repSquare = '#800000';
+    const svgBackgroundColor = '#ffffff';
 
     //width and height of the SVG
     const width = 800;
     const height = 400;
 
     //create an svg with width and height
-    var svg = d3
+    const svg = d3
         .select('#grid-chart')
         .append('svg')
         .attr('width', width)
@@ -18,28 +17,28 @@ var drawGraph = function () {
         .style('background-color', svgBackgroundColor);
 
     //dem_data
-    var demNumRows = 12;
-    var demNumCols = 20;
+    const demNumRows = 12;
+    const demNumCols = 20;
 
     //rep_data
-    var repNumRows = 2;
-    var repNumCols = 4;
+    const repNumRows = 2;
+    const repNumCols = 4;
 
     //x and y axis scales
-    var y = d3.scaleBand().range([0, 200]).domain(d3.range(demNumRows));
+    const y = d3.scaleBand().range([0, 200]).domain(d3.range(demNumRows));
 
-    var x = d3.scaleBand().range([0, 360]).domain(d3.range(demNumCols));
+    const x = d3.scaleBand().range([0, 360]).domain(d3.range(demNumCols));
 
     //the data is just an array of numbers for each cell in the grid
-    var demData = d3.range(demNumRows * demNumCols);
+    const demData = d3.range(demNumRows * demNumCols);
 
-    var repData = d3.range(repNumRows * repNumCols);
+    const repData = d3.range(repNumRows * repNumCols);
 
     const demTranslate =
         'translate(' + width * 0.43 + ',' + height * 0.22 + ')';
 
     //container to hold the grid
-    var demContainer = svg.append('g').attr('transform', demTranslate);
+    const demContainer = svg.append('g').attr('transform', demTranslate);
 
     demContainer
         .selectAll('square')
@@ -64,7 +63,7 @@ var drawGraph = function () {
     const repTranslate =
         'translate(' + width * 0.19 + ',' + height * 0.643 + ')';
 
-    var repContainer = svg.append('g').attr('transform', repTranslate);
+    const repContainer = svg.append('g').attr('transform', repTranslate);
 
     repContainer
         .selectAll('square')
@@ -86,8 +85,7 @@ var drawGraph = function () {
         .attr('fill', repSquare)
         .style('stroke', repSquare);
 
-    var title = svg
-        .append('text')
+    svg.append('text')
         .attr('class', 'title')
         .attr('x', 100)
         .attr('y', 40)
@@ -98,8 +96,7 @@ var drawGraph = function () {
             'Between 2015 and 2023, University of Chicago faculty have donated...'
         );
 
-    var title = svg
-        .append('text')
+    svg.append('text')
         .attr('class', 'rep-text')
         .attr('x', 190)
         .attr('y', 310)
@@ -112,16 +109,14 @@ var drawGraph = function () {
         .style('fill', 'black')
         .text(' to Republican');
 
-    var title = svg
-        .append('text')
+    svg.append('text')
         .attr('class', 'rep-text')
         .attr('x', 190)
         .attr('y', 330)
         .attr('text-anchor', 'middle')
         .text('candidates and PACs');
 
-    var title = svg
-        .append('text')
+    svg.append('text')
         .attr('class', 'rep-text')
         .attr('x', 525)
         .attr('y', 310)
@@ -134,8 +129,7 @@ var drawGraph = function () {
         .style('fill', 'black')
         .text(' to Democratic');
 
-    var title = svg
-        .append('text')
+    svg.append('text')
         .attr('class', 'rep-text')
         .attr('x', 525)
         .attr('y', 330)
