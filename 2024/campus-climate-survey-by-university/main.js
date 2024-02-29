4a617fc301e26944fb7b157cb3f54bb98aaac328
Nikhil Patel <30605884+Nik4002@users.noreply.github.com>
2024-02-29 16:00:06 -0600

    Add campus climate survey visual (#67)
    <!--- 
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Fix issue in the dispatcher where…", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [x ] This visual was approved by the lead developer.
    - [x] The visual was approved by the story's stakeholders.
    
    <img width="664" alt="Screen Shot 2024-02-28 at 9 27 50 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/30605884/bb2cac03-e1f6-42a9-9253-4f5945ffaab6">

---
+++ 2024/campus-climate-survey-by-university/main.js
@@ -0,0 +1,62 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.chart('chart-div', {
+    colors: [
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    // All code for your chart goes here
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Comparative Response Rates in University Campus Climate Surveys',
+        align: 'center'
+    },
+
+    caption: {
+        text: '*Stanford reported a 29-31% response rate from students.',
+        align: 'center'
+    },
+
+    xAxis: {
+        categories: ['UChicago', 'MIT', 'Stanford*', 'Brown']
+    },
+
+    yAxis: {
+        min: 0,
+        max: 100,
+        title: {
+            text: 'Response rate (%)'
+        }
+    },
+
+    tooltip: {
+        valueSuffix: '%'
+    },
+
+    series: [
+        {
+            name: 'Students',
+            data: [21, 39, 30, 16]
+        },
+        {
+            name: 'Academics/Faculty',
+            data: [41, 69, 38, 33]
+        },
+        {
+            name: 'Staff',
+            data: [42, 49, 44, 62]
+        }
+    ]
+});
