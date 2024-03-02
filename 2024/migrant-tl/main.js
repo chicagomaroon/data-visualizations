9caa7dc43c7f0ad32134acc8c8ac5462a69186f5
tiffanyl9 <117232143+tiffanyl9@users.noreply.github.com>
2024-03-01 22:02:26 -0600

    TL version, tried to make circle markers

---
+++ 2024/migrant-tl/main.js
@@ -0,0 +1,361 @@
+var DATA = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            id: '0',
+            type: 'Feature',
+            properties: {
+                name: 'AIC',
+                description: '<strong>AIC</strong>',
+                icon: 'lodging',
+                migrants: 10000
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6464061, 41.9550208, 0.0]
+            }
+        },
+        {
+            id: '1',
+            type: 'Feature',
+            properties: {
+                name: 'Brands Park',
+                description: '<strong>Brands Park</strong>',
+                icon: 'lodging',
+                migrants: 5000
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6992049, 41.9411802, 0.0]
+            }
+        },
+        {
+            id: '2',
+            type: 'Feature',
+            properties: {
+                name: 'Broadway Armory Park',
+                description: '<strong>Broadway Armory Park</strong>',
+                icon: 'lodging',
+                migrants: 75000
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6598587, 41.9891929, 0.0]
+            }
+        }/**,
+        {
+            id: '3',
+            type: 'Feature',
+            properties: {
+                name: 'Casa Esperanza',
+                description: '<strong>Casa Esperanza</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7151664, 41.8659924, 0.0]
+            }
+        },
+        {
+            id: '4',
+            type: 'Feature',
+            properties: {
+                name: 'Chicago Lake Shore Hotel',
+                description: '<strong>Chicago Lake Shore Hotel</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.5856401, 41.8059408, 0.0]
+            }
+        },
+        {
+            id: '5',
+            type: 'Feature',
+            properties: {
+                name: 'Daley College',
+                description: '<strong>Daley College</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7228798, 41.7570501, 0.0]
+            }
+        },
+        {
+            id: '6',
+            type: 'Feature',
+            properties: {
+                name: 'Gage Park',
+                description: '<strong>Gage Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7035777, 41.7935203, 0.0]
+            }
+        },
+        {
+            id: '7',
+            type: 'Feature',
+            properties: {
+                name: 'Halsted Shelter',
+                description: '<strong>Halsted Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6457439, 41.851402, 0.0]
+            }
+        },
+        {
+            id: '8',
+            type: 'Feature',
+            properties: {
+                name: 'Inn of Chicago',
+                description: '<strong>Inn of Chicago</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6230694, 41.8928452, 0.0]
+            }
+        },
+        {
+            id: '9',
+            type: 'Feature',
+            properties: {
+                name: 'Leone Beach Park',
+                description: '<strong>Leone Beach Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.662313, 42.0132045, 0.0]
+            }
+        },
+        {
+            id: '10',
+            type: 'Feature',
+            properties: {
+                name: 'MWRD',
+                description: '<strong>MWRD</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7058692, 41.9764379, 0.0]
+            }
+        },
+        {
+            id: '11',
+            type: 'Feature',
+            properties: {
+                name: 'Near South',
+                description: '<strong>Near South</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.62288, 41.8302083, 0.0]
+            }
+        },
+        {
+            id: '12',
+            type: 'Feature',
+            properties: {
+                name: 'New Life Community Church Lakeview',
+                description:
+                    '<strong>New Life Community Church Lakeview</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6787227, 41.9358319, 0.0]
+            }
+        },
+        {
+            id: '13',
+            type: 'Feature',
+            properties: {
+                name: 'North Park Village',
+                description: '<strong>North Park Village</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7238491, 41.9838682, 0.0]
+            }
+        },
+        {
+            id: '14',
+            type: 'Feature',
+            properties: {
+                name: 'Ogden Shelter',
+                description: '<strong>Ogden Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6619782, 41.8877935, 0.0]
+            }
+        },
+        {
+            id: '15',
+            type: 'Feature',
+            properties: {
+                name: 'Parthenon Guest House (Hostel)',
+                description: '<strong>Parthenon Guest House (Hostel)</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6474612, 41.8775112, 0.0]
+            }
+        },
+        {
+            id: '16',
+            type: 'Feature',
+            properties: {
+                name: 'Piotrowski (Lillian) Park',
+                description: '<strong>Piotrowski (Lillian) Park</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.7309295, 41.8348342, 0.0]
+            }
+        },
+        {
+            id: '17',
+            type: 'Feature',
+            properties: {
+                name: 'Social Club',
+                description: '<strong>Social Club</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.629022, 41.877706, 0.0]
+            }
+        },
+        {
+            id: '18',
+            type: 'Feature',
+            properties: {
+                name: 'Super 8',
+                description: '<strong>Super 8</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6646751, 42.0142016, 0.0]
+            }
+        },
+        {
+            id: '19',
+            type: 'Feature',
+            properties: {
+                name: 'Wadsworth',
+                description: '<strong>Wadsworth</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.5980831, 41.7784533, 0.0]
+            }
+        },
+        {
+            id: '20',
+            type: 'Feature',
+            properties: {
+                name: 'Walnut Shelter',
+                description: '<strong>Walnut Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6686713, 41.8863555, 0.0]
+            }
+        },
+        {
+            id: '21',
+            type: 'Feature',
+            properties: {
+                name: 'West Lake Street Shelter',
+                description: '<strong>West Lake Street Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.651114, 41.8854717, 0.0]
+            }
+        },
+        {
+            id: '22',
+            type: 'Feature',
+            properties: {
+                name: 'North Western Shelter',
+                description: '<strong>North Western Shelter</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.687129, 41.8916781, 0.0]
+            }
+        },
+        {
+            id: '23',
+            type: 'Feature',
+            properties: {
+                name: 'YMCA High Ridge',
+                description: '<strong>YMCA High Ridge</strong>',
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6914618, 42.0125928, 0.0]
+            }
+        },
+        {
+            id: '24',
+            type: 'Feature',
+            properties: {
+                name: "Young Women's Leadership Academy",
+                description:
+                    "<strong>Young Women's Leadership Academy</strong>",
+                icon: 'lodging'
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [-87.6184451, 41.8444336, 0.0]
+            }
+        }*/
+    ]
+};
+
+
+//coordinates correct
+var map = L.map('map').setView([41.88, -87.61], 11)//;
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); 
+
+/**L.geoJSON(states, {
+    style: function(feature) {
+        return {radius: feature.properties.migrants}; 
+    }
+}).addTo(map);
+*/
+
+L.geoJSON(DATA, {
+    style: function (feature) {
+        return L.circleMarker(
+            {latlng: feature.geometry.coordinates}, 
+            {radius: feature.properties.migrants}
+            );
+    }
+}).addTo(map);
+//L.geoJSON(DATA).addTo(map);
