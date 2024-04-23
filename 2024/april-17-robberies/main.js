ae1d0b7d9a0966a34b121f4545e58748e4945a5f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-04-22 22:25:56 -0500

    Update focal point

--- 2024/april-17-robberies/main.js
+++ 2024/april-17-robberies/main.js
@@ -99,7 +99,7 @@ const mapStyles = [
 async function createMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
-        center: new google.maps.LatLng(41.794165267893135, -87.5949018028447),
+        center: new google.maps.LatLng(41.79329339082013, -87.59488302738342),
         mapTypeId: 'terrain',
         styles: mapStyles
     });
