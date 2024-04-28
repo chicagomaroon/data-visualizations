5be2dcec4bb6ae240a46a13d61bd46c008a4bea1
tiffanyl9 <117232143+tiffanyl9@users.noreply.github.com>
2024-04-27 22:36:13 -0500

    Add CO'24 Grad Issue visuals (#78)
    Add Grad Issue class of 2024 charts.
    
    ## What does this PR do?
    Add Grad Issue Class of 2024 pie (employment by industry) and bar
    (employment vs. continuing education) charts.
    
    Also add map!
    
    <img width="654" alt="Screenshot 2024-04-26 at 2 27 35 PM"
    src="https://github.com/chicagomaroon/data-visualizations/assets/45241656/1109399f-c858-4275-bf13-053b11d0921b">
    
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [ ] This visual was approved by the lead developer.
    - [x] The visual was approved by the story's stakeholders.
    
    <!-- Replace this comment with a screenshot of your visual. -->
    
    ---------
    
    Co-authored-by: Asteinhart <45241656+Asteinhart@users.noreply.github.com>
    Co-authored-by: Nik4002 <pateln4002@gmail.com>

---
+++ 2024/classof2024_tl/main.js
@@ -0,0 +1,91 @@
+Highcharts.chart('chart-div', {
+    colors: [
+        '#800000',
+        '#FFA319',
+        '#8F3931',
+        '#5c071e',
+        '#C16622',
+        '#f2ca46',
+        '#a33737',
+        '#de986d',
+        '#4b2163',
+        '#961d1d'
+    ],
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Employment by Industry'
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [
+                {
+                    enabled: true,
+                    distance: 20
+                },
+                {
+                    enabled: true,
+                    distance: -40,
+                    format: '{point.percentage:.1f}%',
+                    style: {
+                        fontSize: '0.8em',
+                        textOutline: 'none',
+                        opacity: 1
+                    },
+                    filter: {
+                        operator: '>',
+                        property: 'percentage',
+                        value: 10
+                    }
+                }
+            ]
+        }
+    },
+    series: [
+        {
+            name: 'Percentage',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Financial Services',
+                    y: 30
+                },
+                {
+                    name: 'Management Consulting',
+                    y: 15
+                },
+                {
+                    name: 'Tech Industry',
+                    y: 12
+                },
+                {
+                    name: 'Science (Industry and Research)',
+                    y: 12
+                },
+                {
+                    name: 'Education',
+                    y: 11
+                },
+                {
+                    name: 'Arts',
+                    y: 6
+                },
+                {
+                    name: 'Healthcare',
+                    y: 5
+                },
+                {
+                    name: 'Startups',
+                    y: 4
+                }
+            ]
+        }
+    ]
+});
---
+++ 2024/grad-issue-map/main.js
@@ -0,0 +1,127 @@
+(async () => {
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/countries/us/us-all.topo.json'
+    ).then((response) => response.json());
+
+    colors = ['#fee5d9', '#fcae91', '#fb6a4a', '#de2d26', '#a50f15'];
+
+    const data = [
+        { region: 'West', state_code: 'AK', value: 13 },
+        { region: 'South', state_code: 'AL', value: 9 },
+        { region: 'South', state_code: 'AR', value: 9 },
+        { region: 'West', state_code: 'AZ', value: 13 },
+        { region: 'West', state_code: 'CA', value: 13 },
+        { region: 'West', state_code: 'CO', value: 13 },
+        { region: 'Northeast', state_code: 'CT', value: 35 },
+        { region: 'South', state_code: 'DC', value: 9 },
+        { region: 'South', state_code: 'DE', value: 9 },
+        { region: 'South', state_code: 'FL', value: 9 },
+        { region: 'South', state_code: 'GA', value: 9 },
+        { region: 'West', state_code: 'HI', value: 13 },
+        { region: 'Midwest', state_code: 'IA', value: 36 },
+        { region: 'West', state_code: 'ID', value: 13 },
+        { region: 'Midwest', state_code: 'IL', value: 36 },
+        { region: 'Midwest', state_code: 'IN', value: 36 },
+        { region: 'Midwest', state_code: 'KS', value: 36 },
+        { region: 'South', state_code: 'KY', value: 9 },
+        { region: 'South', state_code: 'LA', value: 9 },
+        { region: 'Northeast', state_code: 'MA', value: 35 },
+        { region: 'South', state_code: 'MD', value: 9 },
+        { region: 'Northeast', state_code: 'ME', value: 35 },
+        { region: 'Midwest', state_code: 'MI', value: 36 },
+        { region: 'Midwest', state_code: 'MN', value: 36 },
+        { region: 'Midwest', state_code: 'MO', value: 36 },
+        { region: 'South', state_code: 'MS', value: 9 },
+        { region: 'West', state_code: 'MT', value: 13 },
+        { region: 'South', state_code: 'NC', value: 9 },
+        { region: 'Midwest', state_code: 'ND', value: 36 },
+        { region: 'Midwest', state_code: 'NE', value: 36 },
+        { region: 'Northeast', state_code: 'NH', value: 35 },
+        { region: 'Northeast', state_code: 'NJ', value: 35 },
+        { region: 'West', state_code: 'NM', value: 13 },
+        { region: 'West', state_code: 'NV', value: 13 },
+        { region: 'Northeast', state_code: 'NY', value: 35 },
+        { region: 'Midwest', state_code: 'OH', value: 36 },
+        { region: 'South', state_code: 'OK', value: 9 },
+        { region: 'West', state_code: 'OR', value: 13 },
+        { region: 'Northeast', state_code: 'PA', value: 35 },
+        { region: 'Northeast', state_code: 'RI', value: 35 },
+        { region: 'South', state_code: 'SC', value: 9 },
+        { region: 'Midwest', state_code: 'SD', value: 36 },
+        { region: 'South', state_code: 'TN', value: 9 },
+        { region: 'South', state_code: 'TX', value: 9 },
+        { region: 'West', state_code: 'UT', value: 13 },
+        { region: 'South', state_code: 'VA', value: 9 },
+        { region: 'Northeast', state_code: 'VT', value: 35 },
+        { region: 'West', state_code: 'WA', value: 13 },
+        { region: 'Midwest', state_code: 'WI', value: 36 },
+        { region: 'South', state_code: 'WV', value: 9 },
+        { region: 'West', state_code: 'WY', value: 13 }
+    ];
+
+    // Instantiate the map
+    Highcharts.mapChart('chart-div', {
+        chart: {
+            map: topology
+        },
+
+        title: {
+            text: 'Where is the Class of 2024 Going?'
+        },
+        subtitle: {
+            text: 'Most students are staying in the Mideast or heading to the Northeast.'
+        },
+        legend: {
+            enabled: false
+        },
+        mapNavigation: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        colorAxis: {
+            dataClasses: [
+                {
+                    from: 36,
+                    to: 36,
+                    color: colors[4],
+
+                    name: 'Midwest'
+                },
+                {
+                    from: 35,
+                    to: 35,
+                    color: colors[3],
+                    name: 'Northeastern'
+                },
+                {
+                    from: 13,
+                    to: 13,
+                    color: colors[2],
+                    name: 'West'
+                },
+                {
+                    from: 9,
+                    to: 9,
+                    color: colors[1],
+                    name: 'South'
+                }
+            ]
+        },
+        series: [
+            {
+                data: data,
+                joinBy: ['postal-code', 'state_code'],
+                dataLabels: {
+                    enabled: false
+                },
+                states: {
+                    hover: {
+                        enabled: false
+                    }
+                }
+            }
+        ]
+    });
+})();
