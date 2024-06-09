082b3011f92798e81b0879b9f4607521beb07341
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-06-09 15:34:03 -0500

    craete config functions

---
+++ 2024/property-scroller/js/config.js
@@ -0,0 +1,46 @@
+const config = {
+    chapter1: {
+        title: 'Chapter 1: Isolation: 1890-1930',
+        id: 'chapter1',
+        subsections: [
+            {
+                id: '1.1',
+                text: 'This is the section 1.1',
+                start_year: 1890,
+                end_year: 1900
+            },
+            {
+                id: '1.2',
+                text: 'This is the section 1.2',
+                start_year: 1900,
+                end_year: 1910
+            },
+            {
+                id: '1.3',
+                text: 'This is the section 1.3',
+                start_year: 1910,
+                end_year: 1930
+            }
+        ]
+    },
+    chapter2: {
+        title: 'Chapter 2: The Problem of our Property:” Racially-Restrictive Covenants, 1933–1948',
+        id: 'chapter2',
+        subsections: [
+            {
+                id: '2.1',
+                text: 'This is the section 2.1',
+                start_year: 1930,
+                end_year: 1940
+            },
+            {
+                id: '2.2',
+                text: 'This is the section 2.2',
+                start_year: 1940,
+                end_year: 1950
+            }
+        ]
+    }
+};
+
+sessionStorage.setItem('config', JSON.stringify(config));
