a8db02f9cea2b1f7c0ece41e980c0a462fedcc01
air-kyi <113389784+air-kyi@users.noreply.github.com>
2024-08-23 09:06:49 -0600

    lint everything

--- 2024/common-data-set/main.js
+++ 2024/common-data-set/main.js
@@ -1,91 +1,74 @@
 // For API and chart documentation please look here:
 // https://www.highcharts.com/demo
 Highcharts.setOptions({
-    chart: {
-        type: 'column',
-        // inverted: true,
-        style: {
-            fontFamily: 'Georgia, serif'
+    colors: ['#800000', '#FFA319'],
+
+    xAxis: {
+        categories: [2025, 2026, 2027],
+        title: { text: 'Class of' }
+    },
+
+    yAxis: {
+        labels: {
+            format: '{value}%'
+        },
+        title: {
+            enabled: false
         }
     },
 
-    colors: [
-        '#800000',
-        '#FFA319'
-    ],
-
-    xAxis: {
-        categories: ['Applied','Admitted','Enrolled']
-    },
-    
-    yAxis: {
-        title: {
-            text: 'Percent'
-        },
-
-        stackLabels: {
-            enabled: true,
-            formatter: function(){
-                return this.stack
-            },
-            crop: false,
-            overflow: 'allow',
-            style: {textOutline: 'none'}
-        },
-    },
-
     plotOptions: {
-        column: {
+        series: {
+            pointStart: 2025
+        },
+        area: {
             stacking: 'percent', // percent chart
             dataLabels: {
                 enabled: true,
                 format: '{point.percentage:.0f}%',
-                style: {textOutline: 'none'}
+                style: { textOutline: 'none' }
             }
         }
     }
 });
 
 Highcharts.chart('chart-uchicago', {
-
     // data: {
     //     csvURL: window.location.origin + '/gender.csv'
     // },
 
+    chart: {
+        type: 'line',
+        // inverted: true,
+        style: {
+            fontFamily: 'Georgia, serif'
+        }
+    },
+
     title: {
-        text: 'UChicago applicants by status, incoming year, and gender',
+        text: 'UChicago applicants by incoming year, and gender',
         align: 'center',
         margin: 40
     },
 
     series: [
-        {name:'Male', data:[17513,1254,1067],stack:'2021'},
-        {name:'Female', data:[20445,1206,986],stack:'2021'},
-        {name:'Male', data:[16830,1064,937],stack:'2022',showInLegend:false},
-        {name:'Female', data:[20670,975,792],stack:'2022',showInLegend:false},
-        {name:'Male', data:[17373,994,885],stack:'2023',showInLegend:false},
-        {name:'Female', data:[21249,855,741],stack:'2023',showInLegend:false}         
-    ]
+        { name: 'Male - Accepted', data: [1254, 1064, 994] },
+        { name: 'Female - Accepted', data: [1206, 975, 855] },
+        { name: 'Male - Applied', data: [17513, 16830, 17373] },
+        { name: 'Female - Applied', data: [20445, 20670, 21249] }
+    ],
 
-});
-
-Highcharts.chart('chart-northwestern', {
-    // data: {
-    //     csvURL: window.location.origin + '/gender.csv'
-    // },
-
-    title: {
-        text: 'Northwestern applicants by status, incoming year, and gender',
-        align: 'center',
-        margin: 40
+    accessibility: {
+        point: {
+            valueDescriptionFormat:
+                '{index}. {point.category}, {point.y:,' +
+                '.0f}, {point.percentage:.1f}%.'
+        }
     },
 
-    series: [
-        {name:'Male', data:[21781,1452,925],stack:'2021'},
-        {name:'Female', data:[25855,1869,1161],stack:'2021'},
-        {name:'Male', data:[23534,1652,923],stack:'2022',showInLegend:false},
-        {name:'Female', data:[27725,2043,1115],stack:'2022',showInLegend:false},
-        {name:'Male', data:[24511,1729,965],stack:'2023',showInLegend:false},
-        {name:'Female', data:[27256,2010,1144],stack:'2023',showInLegend:false}           
-    ]
+    tooltip: {
+        pointFormat:
+            '{series.name}: <b>{point.percentage:.1f}%</b> ({point.y:,.0f})<br/>',
+        split: true
+    }
 });
