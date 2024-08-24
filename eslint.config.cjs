62a48f0099922229f108dc6715561ee70d4ffb70
air-kyi <113389784+air-kyi@users.noreply.github.com>
2024-08-23 21:38:43 -0600

    it linted itself?

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,7 @@
-const prettier = require("eslint-plugin-prettier");
-const js = require("@eslint/js");
+const prettier = require('eslint-plugin-prettier');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
     baseDirectory: __dirname,
@@ -11,20 +9,24 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-module.exports = [{
-    ignores: ["**/*.min.js", "**/*bootstrap*", "static_imports/*"],
-}, ...compat.extends("prettier"), {
-    plugins: {
-        prettier,
+module.exports = [
+    {
+        ignores: ['**/*.min.js', '**/*bootstrap*', 'static_imports/*']
     },
+    ...compat.extends('prettier'),
+    {
+        plugins: {
+            prettier
+        },
 
-    languageOptions: {
-        globals: {},
-        ecmaVersion: 8,
-        sourceType: "script",
-    },
+        languageOptions: {
+            globals: {},
+            ecmaVersion: 8,
+            sourceType: 'script'
+        },
 
-    rules: {
-        "prettier/prettier": "error",
-    },
-}];
+        rules: {
+            'prettier/prettier': 'error'
+        }
+    }
+];
