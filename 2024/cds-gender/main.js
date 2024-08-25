f524c050240aef4de2a03b2197d1c49f5b51071a
air-kyi <113389784+air-kyi@users.noreply.github.com>
2024-08-25 16:38:06 -0600

    remove x axis title from gender graphs

--- 2024/cds-gender/main.js
+++ 2024/cds-gender/main.js
@@ -23,8 +23,7 @@ Highcharts.setOptions({
     xAxis: {
         categories: [
             2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
-        ],
-        title: { text: 'Class of' }
+        ]
     },
 
     yAxis: {
