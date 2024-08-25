c659fc9f8bc1e5e672a55d0bc02d4fcbb9bd951e
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-08-25 09:35:54 -0500

    v1 chart

---
+++ 2024/cds-admission/main.js
@@ -0,0 +1,148 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Georgia, serif'
+        }
+    },
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    // All code for your chart goes here
+    title: {
+        text: 'Change in Applicants and Enrollment from 2014 to 2023',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'While both increased, growth in applicants outpaced growth in enrollment.',
+        align: 'center'
+    },
+
+    xAxis: {
+        categories: [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+    },
+    yAxis: {
+        title: {
+            text: ''
+        },
+        labels: {
+            format: '{value:,.0f}'
+        }
+    },
+
+    plotOptions: {
+        series: {
+            lineWidth: 5,
+            marker: {
+                enabled: false
+            }
+        }
+    },
+    annotations: [
+        {
+            draggable: '',
+            labelOptions: {
+                borderRadius: 0,
+                backgroundColor: 'rgba(255, 255, 255, 0)',
+                borderWidth: 0,
+                borderColor: '#AAA'
+            },
+            labels: [
+                {
+                    point: 'aMax',
+                    style: {
+                        fontSize: '16px'
+                    },
+                    text: '<span style="color:#800000"><b>+30% in Applicants from 2014<b>'
+                },
+                {
+                    point: 'eText',
+                    style: {
+                        fontSize: '16px'
+                    },
+                    text: '<span style="color:black"><b>+25% in Enrollment<b>'
+                }
+            ]
+        }
+    ],
+    legend: {
+        enabled: false
+    },
+
+    series: [
+        {
+            name: 'Applications',
+            label: {
+                enabled: false
+            },
+            data: [
+                27500,
+                30069,
+                31484,
+                27694,
+                32283,
+                34641,
+                34350,
+                37958,
+                37500,
+                { y: 38622, id: 'aMax' }
+            ]
+        },
+        {
+            name: 'Enrollment',
+            color: 'black',
+            label: {
+                enabled: false
+            },
+            data: [
+                5608,
+                5800,
+                5976,
+                6295,
+                6600,
+                6801,
+                7011,
+                { y: 7559, id: 'eText' },
+                7512,
+                { y: 7489, id: 'eMax' }
+            ]
+        },
+        {
+            type: 'arearange',
+            data: [
+                [5608, 27500],
+                [5800, 30069],
+                [5976, 31484],
+                [6295, 27694],
+                [6600, 32283],
+                [6801, 34641],
+                [7011, 34350],
+                [7559, 37958],
+                [7512, 37500],
+                [7489, 38622]
+            ],
+            showInLegend: false,
+            enableMouseTracking: false,
+            lineWidth: 0,
+            color: '#80000020'
+        }
+    ]
+});
