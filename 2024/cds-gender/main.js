9dc236f16f339dd24d6cae41eba031276996088a
air-kyi <113389784+air-kyi@users.noreply.github.com>
2024-08-25 16:26:44 -0600

    lint

--- 2024/cds-gender/main.js
+++ 2024/cds-gender/main.js
@@ -21,7 +21,9 @@ Highcharts.setOptions({
     ],
 
     xAxis: {
-        categories: [2014,2015,2016,2017,2018,2019,2020,2021,2022,2023],
+        categories: [
+            2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
+        ],
         title: { text: 'Class of' }
     },
 
@@ -55,21 +57,28 @@ Highcharts.chart('chart-admit', {
     },
 
     series: [
-        { name: 'Male', data: [9.2,9.2,8.5,9.4,7.9,6.8,8.3,7.2,6.3,5.7] },
-        { name: 'Female', data: [8.4,7.7,7.5,8.1,6.7,5.7,6.5,5.9,4.7,4.0] },
-        { name: 'Total', data: [8.8,8.4,7.9,8.7,7.3,6.2,7.3,6.5,5.4,4.8] },
-],
+        {
+            name: 'Male',
+            data: [9.2, 9.2, 8.5, 9.4, 7.9, 6.8, 8.3, 7.2, 6.3, 5.7]
+        },
+        {
+            name: 'Female',
+            data: [8.4, 7.7, 7.5, 8.1, 6.7, 5.7, 6.5, 5.9, 4.7, 4.0]
+        },
+        {
+            name: 'Total',
+            data: [8.8, 8.4, 7.9, 8.7, 7.3, 6.2, 7.3, 6.5, 5.4, 4.8]
+        }
+    ],
 
     accessibility: {
         point: {
-            valueDescriptionFormat:
-                '{index}. {point.category}, {point.y:.1f}%.'
+            valueDescriptionFormat: '{index}. {point.category}, {point.y:.1f}%.'
         }
     },
 
     tooltip: {
-        pointFormat:
-            '{series.name}: {point.y:.1f}%',
+        pointFormat: '{series.name}: {point.y:.1f}%',
         split: true
     }
 });
@@ -94,25 +103,29 @@ Highcharts.chart('chart-percent', {
     },
 
     series: [
-        { name: 'Male', data: [4.4,4.2,3.9,4.4,3.7,3.1,3.8,3.3,2.8,2.6] },
-        { name: 'Female', data: [4.4,4.1,4.1,4.3,3.5,3.1,3.5,3.2,2.6,2.2] },
+        {
+            name: 'Male',
+            data: [4.4, 4.2, 3.9, 4.4, 3.7, 3.1, 3.8, 3.3, 2.8, 2.6]
+        },
+        {
+            name: 'Female',
+            data: [4.4, 4.1, 4.1, 4.3, 3.5, 3.1, 3.5, 3.2, 2.6, 2.2]
+        }
         // { name: 'Total', data: [8.8,8.4,7.9,8.7,7.3,6.2,7.3,6.5,5.4,4.8] }
     ],
 
     yAxis: {
-        tickPositions: [2,2.5,3,3.5,4,4.5],
+        tickPositions: [2, 2.5, 3, 3.5, 4, 4.5]
     },
 
     accessibility: {
         point: {
-            valueDescriptionFormat:
-                '{index}. {point.category}, {point.y:.1f}%.'
+            valueDescriptionFormat: '{index}. {point.category}, {point.y:.1f}%.'
         }
     },
 
     tooltip: {
-        pointFormat:
-            '{series.name}: {point.y:.1f}%',
+        pointFormat: '{series.name}: {point.y:.1f}%',
         split: true
     }
 });
