0403518e69f8dad3017d7d01a4569551f56a55d4
air-kyi <113389784+air-kyi@users.noreply.github.com>
2024-08-25 11:19:18 -0600

    bar graph for race distribution

--- 2024/cds-race-2/main.js
+++ 2024/cds-race-2/main.js
@@ -10,30 +10,30 @@ Highcharts.setOptions({
 
 Highcharts.chart('chart-div', {
     colors: [
-        // '#800000',
-        // '#FFA319',
-        // '#C16622',
-        // '#8F3931',
-        // '#8A9045',
-        // '#58593F',
-        // '#155F83',
-        // '#350E20',
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
         '#47B5FF',
         '#FF3399'
     ],
 
     chart: {
-        type: 'dumbbell',
+        type: 'bar',
         inverted: true
     },
 
     title: {
-        text: 'Change in racial/ethnic distribution of UChicago student body, 2012-2022',
+        text: "Racial/ethnic distribution for UChicago class of '28",
         align: 'center'
     },
 
     subtitle: {
-        text: 'Diversity increased in past ten years, with largest increases in Latino and international students'
+        text: ''
     },
 
     xAxis: {
@@ -41,58 +41,32 @@ Highcharts.chart('chart-div', {
     },
 
     yAxis: {
-        title: {
-            text: 'Enrollment share'
-        },
-        labels: {
-            format: '{value}%'
-        }
+        title: false
     },
+
+    legend: {
+        enabled: false
+    },
+
+    // plotOptions: {
+    //     bar: {
+    //         stacking: 'percent'
+    //     }
+    // },
 
     series: [
         {
-            name: 'Change in enrollment share compared to 2012',
+            name: 'Enrolled students',
             data: [
-                {
-                    name: 'American Indian or Alaska Native',
-                    low: 0.23,
-                    high: 0.07
-                },
-                {
-                    name: 'Asian',
-                    low: 14.14,
-                    high: 15.89
-                },
-                {
-                    name: 'Black or African American',
-                    low: 4.45,
-                    high: 5.29
-                },
-                {
-                    name: 'Hispanic or Latino',
-                    low: 6.15,
-                    high: 10.91
-                },
-                {
-                    name: 'Native Hawaiian or Other Pacific Islanders',
-                    low: 0.01,
-                    high: 0.02
-                },
-                {
-                    name: 'International',
-                    low: 22.16,
-                    high: 30.83
-                },
-                {
-                    name: 'Two or More Races',
-                    low: 0.031,
-                    high: 0.044
-                },
-                {
-                    name: 'White',
-                    low: 49.73,
-                    high: 32.59
-                }
+                {name: "White, non-Hispanic", y: 520},
+                {name: "Asian, non-Hispanic", y: 333},
+                {name: "Hispanic/Latino", y: 313},
+                {name: "International", y: 302},
+                {name: "Two or more races, non-Hispanic", y: 119},
+                {name: "Black or African American, non-Hispanic", y: 115},
+                {name: "Race and/or ethnicity unknown", y: 46},
+                {name: "American Indian or Alaska Native, non-Hispanic", y: 1},
+                {name: "Native Hawaiian or other Pacific Islander, non-Hispanic", y: 0}
             ]
         }
     ],
@@ -100,26 +74,6 @@ Highcharts.chart('chart-div', {
     accessibility: {
         point: {
             valueDescriptionFormat: '{index}. {point.category}, {point.y:.2f}%.'
-        }
-    },
-
-    tooltip: {
-        formatter: function () {
-            var high = this.point.high,
-                low = this.point.low,
-                difference = high - low;
-
-            return (
-                '2022: ' +
-                high +
-                '% <br>' +
-                '2012: ' +
-                low +
-                '% <br>' +
-                'Difference: ' +
-                difference.toFixed(2) +
-                '%'
-            );
         }
     }
 });
