fe0c4c567499414f914f6eeff618d7eeb08c0d00
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-08-28 22:43:49 -0500

    not working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import prettier from "eslint-plugin-prettier";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,20 +12,26 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [{
-    ignores: ["**/*.min.js", "**/*bootstrap*", "static_imports/*"],
-}, ...compat.extends("prettier"), {
-    plugins: {
-        prettier,
+export default [
+    {
+        ignores: ['**/*.min.js', '**/*bootstrap*', 'static_imports/*']
     },
+    ...compat.extends('prettier'),
+    {
+        plugins: {
+            prettier
+        },
 
-    languageOptions: {
-        globals: {},
-        ecmaVersion: 8,
-        sourceType: "script",
-    },
+        languageOptions: {
+            globals: {},
+            ecmaVersion: 8,
+            sourceType: 'module'
+        },
 
-    rules: {
-        "prettier/prettier": "error",
-    },
-}];
+        rules: {
+            'prettier/prettier': 'error'
+        },
+
+        
+    }
+];
