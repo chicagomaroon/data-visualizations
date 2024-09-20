2ed010d1f663e799d08f59a462d1cf99f1ac07a9
Karen Yi <113389784+air-kyi@users.noreply.github.com>
2024-09-20 11:16:16 -0400

    Create second-draft graphics for Common Data Set story (#96)
    <!--- 
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Fix issue in the dispatcher where…", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    - Bar chart for class of '28 race distribution
    - Dumbbell chart for change in overall undergraduate race distribution
    from 2012-2022
    - Line chart for change in gender distribution for admitted students
    2012-2023. 2 options for this with different calculation methods
    - method 1: numerator=women admitted, denominator=women applied. this
    shows that less women were admitted than men consistently every year
    since 2012. however, more women have applied every year since 2012 than
    men.
    - method 2: controlling for number of people applying per group.
    numerator=women admitted. denominator=ALL applicants. this shows that
    more women were admitted than men in 2016 but even proportional to the
    student body as a whole, more men were admitted most years. I think this
    is a more condemning conclusion but more complex than 1.
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [x] This visual was approved by the lead developer.
    - [x] The visual was approved by the story's stakeholders.
    
    
    ![image](https://github.com/user-attachments/assets/23776a74-bdd6-40e5-b9e9-5b279800d594)
    
    
    ![image](https://github.com/user-attachments/assets/e9ce59fc-d5c0-4c98-bc2b-7029e2408c7a)
    
    
    ![image](https://github.com/user-attachments/assets/326acdd8-a6c7-49b8-a908-1db1a9a679e8)
    
    ---------
    
    Co-authored-by: Austin Steinhart <asteinhart@uchicago.edu>
    Co-authored-by: Asteinhart <45241656+Asteinhart@users.noreply.github.com>

---
+++ 2024/cds-gender/main.js
@@ -0,0 +1,188 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Georgia, serif'
+        }
+    },
+
+    colors: [
+        // '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#800000',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    xAxis: {
+        categories: [
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            ''
+        ],
+        endOnTick: false,
+        max: 10
+    },
+
+    yAxis: {
+        labels: {
+            format: '{value}%'
+        },
+        title: {
+            enabled: false
+        }
+    }
+});
+
+Highcharts.chart('chart-admit', {
+    // data: {
+    //     csvURL: window.location.origin + '/gender.csv'
+    // },
+
+    chart: {
+        type: 'line'
+    },
+
+    title: {
+        text: 'Admission Rates by Gender from 2014-2023',
+        align: 'center',
+        margin: 40
+    },
+
+    subtitle: {
+        text: 'Female admission rate is lower than male'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    plotOptions: {
+        line: {
+            marker: false
+        }
+    },
+
+    annotations: [
+        {
+            labels: [
+                {
+                    point: 'mmax',
+                    text: 'Male',
+                    y: 15,
+                    style: {
+                        color: '#C16622'
+                    }
+                },
+                {
+                    point: 'fmax',
+                    text: 'Female',
+                    y: 20,
+                    style: {
+                        color: '#8F3931'
+                    }
+                },
+                {
+                    point: 'omax',
+                    text: 'Overall',
+                    y: 15,
+                    style: {
+                        color: '#800000'
+                    }
+                }
+            ],
+            labelOptions: {
+                draggable: '',
+                color: 'black',
+                backgroundColor: 'none',
+                borderColor: 'none',
+                align: 'right',
+                x: 70,
+                style: {
+                    fontSize: 14,
+                    fontWeight: 'bold'
+                }
+            }
+        }
+    ],
+
+    series: [
+        {
+            name: 'Male',
+            data: [
+                9.2,
+                9.2,
+                8.5,
+                9.4,
+                7.9,
+                6.8,
+                8.3,
+                7.2,
+                6.3,
+                { y: 5.7, id: 'mmax' }
+            ],
+            lineWidth: 2,
+            dashStyle: 'Dash',
+            id: 'male'
+        },
+        {
+            name: 'Female',
+            data: [
+                8.4,
+                7.7,
+                7.5,
+                8.1,
+                6.7,
+                5.7,
+                6.5,
+                5.9,
+                4.7,
+                { y: 4.0, id: 'fmax' }
+            ],
+            lineWidth: 2,
+            dashStyle: 'Dot'
+        },
+        {
+            name: 'Overall',
+            data: [
+                8.8,
+                8.4,
+                7.9,
+                8.7,
+                7.3,
+                6.2,
+                7.3,
+                6.5,
+                5.4,
+                { y: 4.8, id: 'omax' }
+            ],
+            lineWidth: 3,
+            dashStyle: 'solid'
+        }
+    ],
+
+    accessibility: {
+        point: {
+            valueDescriptionFormat: '{index}. {point.category}, {point.y:.1f}%.'
+        }
+    },
+
+    tooltip: {
+        pointFormat: '{series.name}: {point.y:.1f}%',
+        split: true
+    }
+});
