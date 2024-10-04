b3c96041161103adb1e65338cb740cb53da4ca09
erikalenh <erika.lenhardt@gmail.com>
2024-10-03 21:06:17 -0500

    Bizecon major (#103)
    <!--- 
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Fix issue in the dispatcher where…", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    Visualize number of biz econ majors and computer science majors.  
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    The chart and its associated pages have been minified using the `npm run
    process-visual` command.
    
    
    <!-- Replace this comment with a screenshot of your visual. -->
    <img width="1226" alt="Screenshot 2024-10-03 at 8 54 53 AM"
    src="https://github.com/user-attachments/assets/3c5b2a92-bfcd-44da-bf18-b4c870e1a92b">
    
    ---------
    
    Co-authored-by: Nik4002 <pateln4002@gmail.com>
    Co-authored-by: Nikhil Patel <30605884+Nik4002@users.noreply.github.com>

---
+++ 2024/bizecon-major/main.js
@@ -0,0 +1,46 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+
+Highcharts.chart('chart-div', {
+    colors: ['#800000', '#FFA319'],
+
+    title: {
+        text: 'Undergraduate Population',
+        align: 'center'
+    },
+
+    caption: {
+        text: '*These data reflect student populations as of Autumn quarter each year.',
+        align: 'center'
+    },
+
+    yAxis: {
+        min: -50,
+        startOnTick: false,
+        title: {
+            text: 'Number of Students'
+        }
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 1979
+        }
+    },
+
+    series: [
+        {
+            name: 'Total Enrollment',
+            data: [
+                2715, 2755, 2867, 2870, 2920, 2981, 2977, 3166, 3254, 3332,
+                3433, 3478, 3447, 3425, 3435, 3478, 3554, 3616, 3756, 3852,
+                3917, 3996, 4064, 4216, 4344, 4515, 4642, 4780, 4894, 5026,
+                5098, 5252, 5369, 5607, 5692, 5724, 5860, 5971, 6286, 6595,
+                6790, 7001, 7550, 7496, 7535, 7398
+            ]
+        }
+    ]
+});
