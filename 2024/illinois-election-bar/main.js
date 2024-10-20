994d290aa24bbc44eaa767c4d9b3e9f0fb25dd87
Karen <notkarenyi@gmail.com>
2024-10-20 10:32:09 -0500

    run prettier

--- 2024/illinois-election-bar/main.js
+++ 2024/illinois-election-bar/main.js
@@ -36,7 +36,7 @@ Highcharts.chart('chart-div', {
     },
 
     subtitle: {
-        text: "State Senate campaign raised half as much as other campaigns",
+        text: 'State Senate campaign raised half as much as other campaigns',
         align: 'center',
         style: {
             fontSize: '18px'
