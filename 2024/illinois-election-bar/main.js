db2c8f88ce55b8c98f253d2088d96a3ec46ab08e
Leah Vashevko <leahvashevko@gmail.com>
2024-10-20 23:30:14 -0500

    Add 2024 Illinois Election Pie Charts (#106)
    <!--- 
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Fix issue in the dispatcher where…", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    
    Add visuals for the 2024 Illinois Election campaign contribution data.
    
    See screenshots below of each graph:
    
    <img width="697" alt="image"
    src="https://github.com/user-attachments/assets/6b160d34-f499-4a9f-9b32-a37943f4ebc3">
    
    <img width="697" alt="image"
    src="https://github.com/user-attachments/assets/9d3158d2-fe7c-451d-a153-66a26ce738fd">
    
    <img width="699" alt="image"
    src="https://github.com/user-attachments/assets/6dfeaa10-6476-4563-bfa5-793a23d77832">
    
    
    
    
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of the `html` page is the same as the chart.
    - [x] The `meta_data.json` file is filled in correctly (the description
    is a complete sentence, the title is the same as the visual).
    - [x] The chart and its associated pages have been minified using the
    `npm run process-visual` command.
    - [x] The visual is styled similar to other charts (centered
    title/subtitle, no legends unless absolutely necessary, etc.)
    - [ ] This visual was approved by the lead developer.
    - [ ] The visual was approved by the story's stakeholders.
    
    <!-- Replace this comment with a screenshot of your visual. -->
    
    ---------
    
    Co-authored-by: Karen <notkarenyi@gmail.com>
    Co-authored-by: Nik4002 <pateln4002@gmail.com>

---
+++ 2024/illinois-election-bar/main.js
@@ -0,0 +1,104 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Georgia, serif'
+        }
+    },
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#800000',
+        '#FFA319',
+        '#C16622',
+        '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    chart: {
+        type: 'bar'
+    },
+
+    title: {
+        text: 'Total Donations by Candidate and Race',
+        align: 'center'
+    },
+
+    subtitle: {
+        text: 'Candidates with known donation information raised $464K on average.',
+        align: 'center',
+        style: {
+            fontSize: '18px'
+        }
+    },
+
+    tooltip: {
+        formatter: function () {
+            return (
+                '<b>' +
+                this.point.name +
+                '</b>' +
+                ': ' +
+                '$' +
+                Highcharts.numberFormat(this.y, 0, ',')
+            );
+        },
+        valuePrefix: '$'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    xAxis: {
+        type: 'category'
+    },
+
+    yAxis: {
+        labels: {
+            format: '${text}',
+            style: {
+                fontSize: '16px'
+            }
+        },
+        tickPositions: [
+            0, 100000, 200000, 300000, 400000, 500000, 600000, 650000
+        ],
+        showLastLabel: false,
+        title: {
+            enabled: false
+        }
+    },
+
+    plotOptions: {
+        series: {
+            groupPadding: 0,
+            pointPadding: 0.05
+        }
+    },
+
+    series: [
+        {
+            name: 'Total donations',
+            label: {
+                enabled: false
+            },
+            data: [
+                { name: 'Jonathan Jackson (U.S. House)', y: 578000 },
+                { name: 'Kambium Buckner (IL House)', y: 543076 },
+                { name: 'Robert Peters (IL Senate)', y: 271134 }
+            ]
+        }
+    ]
+});
