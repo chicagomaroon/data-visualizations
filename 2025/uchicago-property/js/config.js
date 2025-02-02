7438fc6a0078b0d34260f1b4a1367d2a81bf00f7
Austin Steinhart <asteinhart@uchicago.edu>
2025-02-01 20:25:18 -0500

    Update property to final for pub (#133)
    <!--- 
    This Pull Request should contain all visuals related to one story.
    
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Create visuals for _____ story", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    final_postcopy_final_v1
    
    <!---
    If this PR is for data visualizations you made, please follow these
    steps. Otherwise, please delete all of the text below.
    -->
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [x] The title of each visual's `html` page is the same as the chart
    title.
    - [x] The `meta_data.json` file for each visual is filled in correctly
    (the description is a complete sentence, and the title is the same as
    the visual).
    - [x] Each visual and associated files has been minified using the `npm
    run process-visual` command.
    - [x] Each visual was styled according to style guidelines (centered
    title/subtitle, uses our color scheme, etc.).
    - [x] Each visual was approved by the lead developer.
    - [x] Each visual was approved by the story's authors and/or
    stakeholders.
    - [x] Each visual was submitted to Copy, and the resulting
    comments/edits have been resolved.
    
    <!-- Replace this comment with a screenshot of your visual. -->

--- 2025/uchicago-property/js/config.js
+++ 2025/uchicago-property/js/config.js
@@ -70,7 +70,7 @@ const config = [
             {
                 id: '2.1',
                 text: `According to historian Arnold R. Hirsch, between 1933 and 1947, the University spent $83,597 to support legal assistance for the defense of racially restrictive covenants. These private agreements prohibited African Americans from buying property and living in certain neighborhoods, like Hyde Park. The University accomplished this goal by subsidizing neighborhood groups advocating for the covenants, such as the Woodlawn Property Owners League and the Hyde Park Property Owners Association. University President Hutchins maintained that residents of Hyde Park and nearby communities had the right to “invoke and defend” racially restrictive covenants as legal instruments.`,
-                quote: `“[The University] must endeavor to stabilize its neighborhood as an area in which its students and faculty will be content to live.”`,
+                quote: `"[The University] must endeavor to stabilize its neighborhood as an area in which its students and faculty will be content to live."`,
                 quoteAuthor:
                     'President Maynard Hutchins, responding to <i>Chicago Defender</i> coverage in 1937',
                 quoteSource: ''
@@ -98,15 +98,15 @@ const config = [
                 id: '3.0',
                 text: `
                 With the outlawing of racial covenants, the Black population in the University’s immediate surroundings continued to increase: Hyde Park had 573 Black residents in 1940 and 1,757 in 1950, most of whom had arrived after 1948, according to Hirsch’s <i>Making the Second Ghetto</i>. Hirsch writes that the University saw maintaining a “desirable” neighborhood as tied to its survival and key to its academic mission. It began substantial property acquisition and “blight removal,” (a term used in the University of Chicago’s 1949 Treasurer’s report) making it the first American higher education institution to embark on a comprehensive urban renewal campaign.`,
-                quote: `“I believe we have found the answer. It is called 'Urban Renewal' ... it is, however, a tragic fact that these efforts are today essential to the ability of the institutions to fulfill their primary responsibilities.”`,
+                quote: `"I believe we have found the answer. It is called 'Urban Renewal' ... it is, however, a tragic fact that these efforts are today essential to the ability of the institutions to fulfill their primary responsibilities."`,
                 quoteAuthor:
-                    'Julian Levi, UChicago professor and South East Chicago Commission (SECC) executive director',
+                    'Julian Levi, South East Chicago Commission (SECC) executive director and later professor at the University of Chicago',
                 quoteSource: 'Haar, The City as Campus'
             },
             {
                 id: '3.0a',
                 text: ``,
-                quote: `Beginning in the early 1940s, signs of deterioration around the University were evident. As a result of the Depression and the war, many buildings had not been maintained for 15 years, and there had been little new investment in the area... The heavy migration of blacks from the South to Chicago resulted in severe population pressures on the South Side... which led to predatory real estate practices: slumlords illegally converting six flats into twenty-four-unit rooming houses charging exorbitant rental rates, and not maintaining buildings to code."`,
+                quote: `"Beginning in the early 1940s, signs of deterioration around the University were evident. As a result of the Depression and the war, many buildings had not been maintained for 15 years, and there had been little new investment in the area... The heavy migration of blacks from the South to Chicago resulted in severe population pressures on the South Side... which led to predatory real estate practices: slumlords illegally converting six flats into twenty-four-unit rooming houses charging exorbitant rental rates, and not maintaining buildings to code."`,
                 quoteAuthor: `Former University Dean and Senior Advisor to the University President John Boyer, <i>The University of Chicago: A History</i>`,
                 quoteSource: ''
             },
@@ -144,13 +144,13 @@ const config = [
                 id: '3.5a',
                 text: `Urban renewal fueled unrest across the city. In 1955, Black residents marched on city hall and demanded that Mayor Richard Daley address the discrimination against Black families, writes Eddie R. Cole in <i>The Campus Color Line</i>. Local leaders also raised concerns about the University’s role. “I see this whole area being controlled by a monolithic institution via its political puppets,” accused Wesley W. South, candidate for the 2nd Congressional district in 1968 in the <i>Chicago Defender</i>.`,
                 image: './static/images/ousts.png',
-                image_credit: 'Chicago Defender, 1962'
+                image_credit: '<i>Chicago Defender</i>, 1962'
             },
             {
                 id: '3.5b',
                 text: ``,
-                quote: `Land clearance is perverted to Negro Clearance. Urban Renewal, despite its good intentions and its great possibilities, becomes a distortion and a false promise. Slums have not been eradicated but have been made mobile. Most of the new housing units erected, though providing good living, have not provided housing for those displaced.”`,
-                quoteAuthor: `Edwin C. Berry, Executive Director of the Chicago Urban League, 1962`,
+                quote: `"Land clearance is perverted to Negro Clearance. Urban Renewal, despite its good intentions and its great possibilities, becomes a distortion and a false promise. Slums have not been eradicated but have been made mobile. Most of the new housing units erected, though providing good living, have not provided housing for those displaced."`,
+                quoteAuthor: `Executive Director of the Chicago Urban League Edwin C. Berry, 1962`,
                 quoteSource: ''
             },
 
@@ -159,7 +159,7 @@ const config = [
                 text: `
                 Cumulatively, these plans called for the demolition of 193 acres (20 percent of the Hyde Park); cost $120 million ($1.4 billion when adjusted for inflation), approximately half of which was public funds and the other half private; displaced more than 30,000 people; and enabled the University of Chicago to add 41 acres of land to its campus. The campaign was a success, according to the administration. Documented by Levi, it became a model for other institutions, such as <a href = "https://projects.browndailyherald.com/2022/12/13/brown-property-footprint/" target = "_blank">Brown University</a> in the 1960s and <a href = "https://www.theguardian.com/cities/2018/apr/18/gentrify-or-die-inside-a-universitys-controversial-plan-for-baltimore" target = "_blank">Johns Hopkins</a> in the 2010s.`,
                 quote: `"The Chicago Association of Industry Tuesday endorsed a 50-acre redevelopment project for the Hyde Park area. The association said that the project, planned by the South East Chicago Commission, 'may well become a model for similar urban renewal in other sections of the city.'"`,
-                quoteAuthor: 'Chicago Daily News, 1954',
+                quoteAuthor: '<i>Chicago Daily News</i>, 1954',
                 quoteSource: ''
             }
         ]
@@ -179,7 +179,7 @@ const config = [
             {
                 id: '4.2',
                 text: `
-                In 1957, the University created the UC Bus, the first iteration of the on-campus shuttle system. Marc David Loeb (A.B. ’21) <a href = "https://humanities-web.s3.us-east-2.amazonaws.com/college/chicagostudies-prod/s3fs-public/2021-05/Loeb,%20Marc%20-%20Building%20a%20Selective%20Permeability%20of%20Space%20-%20Urban%20Renewal%20and%20The%20University%20of%20Chicago%20Campus%20Shuttle%20System.pdf" target = "_blank">argued</a> that the shuttle system was “reappropriated as a tool of urban renewal,” granting the University freedom to delineate and extend the borders of campus. The shuttle system has added eight new routes since it was created, including a <a href = "https://www.chicagomaroon.com/article/2021/9/28/ucpd-announces-new-shuttle-route-garfield-green-red/" target = "_blank">new route</a> from campus to the Green and Red Line Garfield stations and the Washington Park Arts Incubator in 2021.`
+                In 1957, the University created the UC Bus, the first iteration of the on-campus shuttle system. Marc David Loeb (A.B. ’21) <a href = "https://humanities-web.s3.us-east-2.amazonaws.com/college/chicagostudies-prod/s3fs-public/2021-05/Loeb,%20Marc%20-%20Building%20a%20Selective%20Permeability%20of%20Space%20-%20Urban%20Renewal%20and%20The%20University%20of%20Chicago%20Campus%20Shuttle%20System.pdf" target = "_blank">argued</a> that the shuttle system was “reappropriated as a tool of urban renewal,” granting the University freedom to delineate and extend the borders of campus. The shuttle system has added eight new routes since it was created, including a <a href = "https://safety-security.uchicago.edu/en/news-alerts/2021-09-03-transportation-parking-announces" target = "_blank">new route</a> from campus to the Green and Red Line Garfield stations and the Washington Park Arts Incubator in 2021.`
             },
             {
                 id: '4.3',
@@ -204,7 +204,6 @@ const config = [
                 id: '4.6',
                 text: `
                 During the early 2000s, the University formulated a new plan to reimagine residential life on campus by constructing new housing, dining, and recreational facilities within walking distance of the central quadrangles. New facilities included a transformed student center in the Reynolds Club and Ratner Athletics Center, the first new athletics facility built on campus since 1935. In 2002, the Max Palevsky Residential Commons were built, beginning a pattern of large dorms built on campus; in subsequent years, three more large dorms were built on campus. These additions brought necessities and leisure <a href = "https://chicagomaroon.com/42601/grey-city/the-case-of-the-checkerboard-blues/" target = "_blank">closer to campus</a>.`
-
             },
             {
                 id: '4.7',
@@ -252,7 +251,7 @@ const config = [
                 The future of the University’s involvement in Hyde Park and the South Side is at a crucial inflection point. 
                 With the <a href = "https://chicagomaroon.com/41032/grey-city/the-obama-center-a-welcome-neighbor/" target = "_blank">opening of the Obama Presidential Center</a> nearing, the University is once again in a position to make decisions that will significantly impact the neighborhood’s future. 
                 
-                The University, while a minor donor to the Center, is poised to benefit from the estimated <a href = "https://www.obama.org/press-releases/obama-presidential-center-estimated-support-thousands-jobs-city-chicago-south-side-construction-ten-years-operations/" target = "_blank">$3.1 billion in “economic impact”</a>. The Center joins large-scale university-owned projects that are expected to impact the South Side in coming years, including <a href = "https://hydeparklabs.com/" target = "_blank">Hyde Park Labs</a> and UCMed’s <a href = "https://news.uchicago.edu/story/state-approves-uchicago-medicines-plan-build-citys-first-freestanding-cancer-care-and" target = "_blank">Cancer Pavilion</a>.
+                The University, while a minor donor to the Center, is poised to benefit from the estimated <a href = "https://www.obama.org/press-releases/obama-presidential-center-estimated-support-thousands-jobs-city-chicago-south-side-construction-ten-years-operations/" target = "_blank">$3.1 billion in “economic impact.”</a> The Center joins large-scale university-owned projects that are expected to impact the South Side in coming years, including <a href = "https://hydeparklabs.com/" target = "_blank">Hyde Park Labs</a> and UCMed’s <a href = "https://news.uchicago.edu/story/state-approves-uchicago-medicines-plan-build-citys-first-freestanding-cancer-care-and" target = "_blank">Cancer Pavilion</a>.
                 While the University has emphasized the positive impact the projects will have on the local economy, some residents are concerned about <a href = "https://www.theguardian.com/us-news/2023/feb/15/obama-center-chicago-south-side-residents-fear-displacement" target = "_blank">rising rent</a>  and how much benefit will reach the community as opposed to <a href = "https://blockclubchicago.org/2022/12/27/as-investors-buy-more-homes-around-the-obama-presidential-center-gentrification-worries-soar/" target = "_blank">investors</a>.
                 `
             },
@@ -268,7 +267,7 @@ const config = [
                 text: `
                 However, recent actions leave many wary of the University’s commitments.
                 The opening of the Woodlawn Charter School in 2018 on 63rd Street broke the University’s <a href = "https://chicagomaroon.com/25412/news/uchicago-charter-schools-opens-new-woodlawn-campus/" target = "_blank">decades-old agreement</a> with The Woodlawn Organization to not develop past 61st Street and further into Woodlawn.
-                In 2020, former President Zimmer and incoming President Paul Alivisatos committed to exploring and addressing the University’s historical relationship with its surrounding neighborhoods by launching the <a href = "https://news.uchicago.edu/story/council-examine-history-relationship-between-university-chicago-and-south-side-communities' target = "_blank">Council on UChicago/Community Relations</a>, tasked with examining the historical relationship between the University and its South Side neighbors. In 2023, after the Council’s first two years, it stated that it was <a href ='https://chicagomaroon.com/40648/news/community-relations-council-tries-to-tackle-public-safety-and-displacement/' target = "_blank">not yet prepared to engage meaningfully with the public</a>, but that outreach could begin soon. There have been no public updates since.`
+                In 2020, former President Zimmer and incoming President Paul Alivisatos committed to exploring and addressing the University’s historical relationship with its surrounding neighborhoods by launching the <a href = "https://news.uchicago.edu/story/council-examine-history-relationship-between-university-chicago-and-south-side-communities">Council on UChicago/Community Relations</a>, tasked with examining the historical relationship between the University and its South Side neighbors. In 2023, after the Council’s first two years, it stated that it was <a href ='https://chicagomaroon.com/40648/news/community-relations-council-tries-to-tackle-public-safety-and-displacement/' target = "_blank">not yet prepared to engage meaningfully with the public</a>, but that outreach could begin soon. There have been no public updates since.`
             },
             {
                 id: 'conclusion',
