d9d3cf429491e40b4ef608b620840395996d4322
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-02-25 10:34:00 -0600

    updates

--- 2025/annotated/js/main.js
+++ 2025/annotated/js/main.js
@@ -1,5 +1,7 @@
 //https://www.nytimes.com/interactive/2025/02/13/us/doc-annotation-memo-from-bove.html
 
+let active_highlight;
+
 document.querySelectorAll('.highlight').forEach((highlight) => {
     highlight.addEventListener('click', (e) => {
         if (highlight.classList.contains('active')) {
@@ -8,4 +10,20 @@ document.querySelectorAll('.highlight').forEach((highlight) => {
             highlight.classList.add('active');
         }
     });
+});
+
+new Waypoint({
+    element: document.querySelector('#highlight-2'),
+    handler: function (direction) {
+        if (direction == 'down') {
+            // make active
+            console.log('highlight-2');
+            active_highlight.classList.remove('active');
+            active_highlight = document.querySelector('#highlight-2');
+            active_highlight.classList.add('active');
+            let annotation = document.querySelector('.annotation-card-text');
+            annotation.innerHTML = 'this is second annoation';
+        }
+    },
+    offset: '60%'
 });
