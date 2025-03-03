d9973fbd1a48f3965046fdee1b00494b2536b62e
Karen <notkarenyi@gmail.com>
2025-03-02 18:21:55 -0600

    support chunk output format
    add documentation
    iterrows to avoid awkward at syntax
    move data manipulation outside of class for more flexibility

--- 2025/divestment-nlp/scrape-letters.py
+++ 2025/divestment-nlp/scrape-letters.py
@@ -1,4 +1,5 @@
 # DO NOT RUN IN IPYTHON KERNEL OR ELSE THE SCRAPE WILL NOT WORK
+# Purpose: Scrape text from Maroon archives hosted at UChicago Libraries
 from __future__ import annotations
 
 import re
@@ -20,13 +21,30 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\notka\AppData\Local\Programs\
 
 class Scraper:
 
-    def __init__(self, input_data):
+    def __init__(self, input_data, chunks=False):
+        """
+        Create a Scraper object and decide if we are outputting just the full text or chunks
+        Chunks are useful for further text analysis within documents
+        """
+
+        if not input_data['Link']:
+            print(
+                f"Please ensure your input file has a column named Link. None found in {input_data.columns}",
+            )
+            raise Exception
+
         self.input_data = input_data
-        self.input_data['Processed'] = False
-        self.links = []
-        self.chunks = []
+        self.input_data['Text'] = None
+        if chunks:
+            self.output_format = 'chunks'
+        else:
+            self.output_format = 'full_text'
 
     def process_site(self, site):
+        """
+        Extract the main content of a vanilla Maroon page, which currently runs on SNO Sites
+        """
+
         r = requests.get(site)
         soup = bs(r.content, 'html.parser')
         try:
@@ -37,69 +55,136 @@ class Scraper:
         tags = story.find_all()
         return story.get_text(), tags
 
-    def process_archive(self, site):
+    def cook_soup(self, site):
         """
+        beautifulsoup can't get rendered JS content, so we use HTMLSession to render for us
 
         Sources:
         https://stackoverflow.com/questions/8049520/how-can-i-scrape-a-page-with-dynamic-content-created-by-javascript-in-python
         https://stackoverflow.com/questions/78023508/python-request-html-is-not-downloading-chromium
         https://stackoverflow.com/questions/59665773/why-render-requests-html-doesnt-scrape-dynamic-content
-        https://stackoverflow.com/questions/71296581/python-beautifulsoup-get-texts-before-a-certain-tag
-        https://stackoverflow.com/questions/37158246/how-to-download-images-from-beautifulsoup
-        https://stackoverflow.com/questions/44057140/loading-image-from-webpage-link-directly-into-numpy-array-python
-        https://stackoverflow.com/questions/43403086/opening-image-file-from-url-with-pil-for-text-recognition-with-pytesseract
         """
 
         session = HTMLSession()
-        r = session.get(site)  # bs can't get js content
+        r = session.get(site)
         try:
             r.html.render(sleep=10)
         except RuntimeError as e:
-            print(
-                f"Did you forget to run this from the command line? Need more sleep for {site}: {e}",
-            )
-            return
+            print(f"Did you forget to run this from the command line?\n{e}")
+            raise e
         soup = bs(r.html.html, 'html.parser')
-        highlights = soup.find_all('div', class_='BookReaderSearchHilite')
         session.close()
+        return soup
+
+    def process_archive(self, site):
+        """
+        Process an archive page, which uses BookReader to display scanned images of archival issues:
+        https://openlibrary.org/dev/docs/bookreader
+
+        TODO: ask librarians if there are easier ways to access text than scraping
+
+        Params:
+            site: (str) URL to a webpage to scrape
+        Output:
+            output: (list) Always in a list, even if containing only one item, to keep data types consistent while supporting the chunks output format
+
+        Sources:
+        https://stackoverflow.com/questions/71296581/python-beautifulsoup-get-texts-before-a-certain-tag
+        https://stackoverflow.com/questions/37158246/how-to-download-images-from-beautifulsoup
+        https://stackoverflow.com/questions/44057140/loading-image-from-webpage-link-directly-into-numpy-array-python
+        """
+
+        soup = self.cook_soup(site)
+        highlights = soup.find_all('div', class_='BookReaderSearchHilite')
 
         if not len(highlights):
-            print(len(soup))
-        else:
-            try:
-                output = ''
-                # print(soup)
-                # print(soup.find('div', id_='BookReader'))
-                for highlight in highlights:
-                    print(highlight)
-                    page = highlight.previous_sibling
-                    print(page['src'])
-                    img = requests.get(page['src'])
-                    image = Image.open(BytesIO(img.content))
-                    # print(image)
-                    text = pytesseract.image_to_string(image)
-                    # print('\n'.join(text.split('\n')[:5]))
-                    print(f"Length of highlight: {len(text)}")
-                    output += text
-                return output
-            except Exception as e:
-                print(e)
+            print(
+                f"No highlights found on page. The length of the soup is {len(soup)}",
+            )
+            return
+
+        try:
+
+            output = []
+
+            # print(soup)
+            # print(soup.find('div', id_='BookReader'))
+            for highlight in highlights:
+                print(highlight)
+                page = highlight.previous_sibling
+                print(f"Found image: {page['src']}")
+                img_bytes = requests.get(page['src'])
+                img = Image.open(BytesIO(img_bytes.content))
+                # print(img)
+
+                if self.output_format == 'chunks':
+                    output += self.get_bboxes(img)
+                else:
+                    output += [self.get_all_text(img)]
+
+            return output
+        except Exception as e:
+            print(e)
+
+    def get_all_text(self, img):
+        """
+        OCR all text from the image of the page using PyTesseract
+
+        Sources:
+        https://stackoverflow.com/questions/43403086/opening-image-file-from-url-with-pil-for-text-recognition-with-pytesseract
+        """
+
+        text = pytesseract.image_to_string(img)
+        # print('\n'.join(text.split('\n')[:5]))
+        print(f"Length of extracted text: {len(text)}")
+        return text
 
     def get_bboxes(self, img):
         """
+        Chunks image using bounding boxes (a computer vision technique)
+        Get text within each chunk by calling get_all_text()
 
-        Sources: https://stackoverflow.com/questions/21104664/extract-all-bounding-boxes-using-opencv-python
+        Sources:
+        https://stackoverflow.com/questions/21104664/extract-all-bounding-boxes-using-opencv-python
         """
 
-        im = cv2.imread(img)
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        contours, hierarchy = cv2.findContours(
+        cv2_img = cv2.imread(img)
+        gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+        contours, _ = cv2.findContours(
             gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE,
         )[-2:]
 
+        chunks = []
+        for i, contour in enumerate(contours):
+            x, y, w, h = cv2.boundingRect(contour)
+            chunk = cv2_img[y:y+h, x:x+w]
+            chunks.append(self.get_all_text(chunk))
+
+        # cv2.waitKey(0)
+        return chunks
+
+    def export_results(self):
+        """
+        Export final results to an Excel and uses log file as fallback (in case of mismatched row numbers or bad characters)
+        """
+
+        # expand the column of lists so that there is 1 row per chunk
+        self.df = self.df.explode('Text')
+
+        try:
+            self.df.to_excel(f'scrape-{str(date.today())}.xlsx', index=False)
+        except Exception as e:
+            with open(f'log-{str(date.today())}.txt', 'w', encoding='utf-8') as f:
+                f.write('\n\n\n'.join(self.df['Text'].values))
+            print(e)
+
     def process_all(self):
+        """
+        Loop through all the URLs listed in the input file
+        """
+
         for i, row in self.df.iterrows():
-            if row['Processed']:
+            if row['Text']:
                 # skip if there is information in the row already (if rerun)
                 continue
             site = row['Link']
@@ -114,23 +199,13 @@ class Scraper:
                 f"Scraping: {site} ({i}/{len(self.df)})----------------------",
             )
             if 'campub' in site:  # for archive pieces, read text from images
-                chunks = self.process_archive(site)
+                row['Text'] = self.process_archive(site)
 
             else:  # for modern pieces, they mostly follow the same format
-                chunks = self.process_site(site)
+                row['Text'] = self.process_site(site)
 
-            # mark as done if processed successfully
-            self.chunks.append(chunks)
-            row['Processed'] = True
-
-        today = re.sub('[^0-9\\-]', '', str(date.today()))
         print('Done scraping!')
-        try:
-            self.df.to_excel(f'scrape-{today}.xlsx', index=False)
-        except Exception as e:
-            with open(f'log-{today}.txt', 'w', encoding='utf-8') as f:
-                f.write('\n\n\n'.join(self.df['Text'].values))
-            print(e)
+        self.export_results()
 
 
 def main():
@@ -138,6 +213,7 @@ def main():
     if sys.argv[0] == 'rerun':
         scraper = Scraper(
             input_data=pd.read_excel('scrape-2025-02-27.xlsx', index_col=None),
+            chunks=True,
         )
     elif not len(sys.argv):
         data = pd.read_csv('data.csv')
@@ -155,7 +231,10 @@ def main():
         ]
         data = data[letter_action or public_response]
 
-        scraper = Scraper(input_data=data.reset_index())
+        scraper = Scraper(
+            input_data=data.reset_index(),
+            chunks=True,
+        )
     else:
         return 'Invalid input, try again'
 
