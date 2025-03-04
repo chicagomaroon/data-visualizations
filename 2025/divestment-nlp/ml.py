7184da50621925045da21766d7a50fe6c6691037
Karen <notkarenyi@gmail.com>
2025-03-03 22:27:36 -0600

    gnn

--- 2025/divestment-nlp/ml.py
+++ 2025/divestment-nlp/ml.py
@@ -111,6 +111,36 @@ df['Args'] = sc.fit_predict(
 # PyG package which enables use of message passing, normalization, etc.
 # also refer to https://www.geeksforgeeks.org/graph-neural-networks-with-pytorch/
 
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        # x: Node feature matrix of shape [num_nodes, in_channels]
+        # edge_index: Graph connectivity matrix of shape [2, num_edges]
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return x
+
+model = GCN(
+    in_channels = 384, # n_features
+    hidden_channels = 16, # param
+    out_channels = 6 # n_clusters
+)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(200):
+    pred = model(data.x, data.edge_index)
+    loss = F.cross_entropy(pred[data.train_mask], data.y[data.train_mask])
+
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
 # %% comparison: use k-means clustering from sklearn
 
 kmeans = KMeans(
