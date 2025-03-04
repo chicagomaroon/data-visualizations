ae97f41b9a08d8109f1d764423abd015afedb361
Karen <notkarenyi@gmail.com>
2025-03-03 19:35:48 -0600

    Update clean.py

--- 2025/divestment-nlp/clean.py
+++ 2025/divestment-nlp/clean.py
@@ -1,19 +1,24 @@
 # Purpose: clean output from scraper before feeding to graph ML pipeline
+from __future__ import annotations
+
+import re
 
 import pandas as pd
-import re
 
 df = pd.read_excel('scrape.xlsx')
 print(df['Text'].head())
-df = df.loc[[not (isinstance(x,float) or len(re.sub('[^A-Za-z]','',x))<80) for x in df['Text']]]
+df = df.loc[[not (isinstance(x, float) or len(
+    re.sub('[^A-Za-z]', '', x)) < 80) for x in df['Text']]]
 
 # get bbox out
-df['BBOX'] = [re.split(' !BBOX! ',x)[0] if 'BBOX' in x else '' for x in df['Text']]
-df['Text'] = [re.split(' !BBOX! ',x)[1] if 'BBOX' in x else x for x in df['Text']]
+df['BBOX'] = [re.split(' !BBOX! ', x)[0]
+              if 'BBOX' in x else '' for x in df['Text']]
+df['Text'] = [re.split(' !BBOX! ', x)[1]
+              if 'BBOX' in x else x for x in df['Text']]
 
 df = df.drop_duplicates('Text')
 
-df.to_excel('scrape.xlsx',index=None)
+df.to_excel('scrape.xlsx', index=None)
 
 # def unpack(row):
 #     # cite: Copilot
