48e4d7655a70e91ead791300b8d0f6fb60c03744
JJ <jdavidoff26@uchicago.edu>
2025-04-10 12:48:07 -0500

    updates to main.js

--- 2025/distribution-locations/main.js
+++ 2025/distribution-locations/main.js
@@ -27,8 +27,7 @@ function initMap() {
 
                 // Create an <img> element for the SVG
                 const img = document.createElement('img');
-                img.src =
-                    '/Users/jonathandavidoff/Documents/GitHub/data-visualizations/2025/distribution-locations/maroon_logo_m_black.svg';
+                img.src = 'maroon_logo_m_black.svg';
                 img.style.width = '32px';
                 img.style.height = '32px';
 
