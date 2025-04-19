7458eed70c677bf0e4f0222a00cb422c1739d46d
Karen <52115905+notkarenyi@users.noreply.github.com>
2025-04-19 15:27:48 -0500

    Add divestment history story (#149)
    <!--- 
    This Pull Request should contain all visuals related to one story.
    
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Create visuals for _____ story", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    
    <!---
    If this PR is for data visualizations you made, please follow these
    steps. Otherwise, please delete all of the text below.
    -->
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [ ] The title of each visual's `html` page is the same as the chart
    title.
    no, as discussed.
    - [x] The `meta_data.json` file for each visual is filled in correctly
    (the description is a complete sentence, and the title is the same as
    the visual).
    - [ ] Each visual and associated files has been minified using the `npm
    run process-visual` command.
    NA
    - [x] Each visual was styled according to style guidelines (centered
    title/subtitle, uses our color scheme, etc.).
    - [ ] Each visual was approved by the lead developer.
    - [x] Each visual was approved by the story's authors and/or
    stakeholders.
    - [ ] Each visual was submitted to Copy, and the resulting
    comments/edits have been resolved.
    
    
    ![image](https://github.com/user-attachments/assets/8362048e-43ed-4b71-9dbb-a15805efdd01)
    
    ---------
    
    Co-authored-by: Asteinhart <45241656+Asteinhart@users.noreply.github.com>

---
+++ 2025/divestment-history/static/js/main.js
@@ -0,0 +1,505 @@
+// ------------------ DATA ------------------
+
+/**
+ * Load data from external URL location
+ * @return {json} jsonData Data to be cleaned in a later step
+ */
+async function fetchData() {
+    try {
+        // get data from external source (github)
+        var response = await fetch('data/data.json');
+
+        var jsonData = await response.json();
+
+        // console.log(jsonData)
+        return jsonData;
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+}
+
+function hideChart() {
+    d3.selectAll('#chart-div')
+        .transition()
+        .duration(200)
+        .style('opacity', 0)
+        .style('display', 'none');
+}
+
+function showChart() {
+    d3.selectAll('#chart-div').style('display', 'block');
+    d3.selectAll('#chart-div').transition().duration(200).style('opacity', 1);
+}
+
+/**
+ * Group data and define all traces
+ * Cite: https://stackoverflow.com/questions/65044430/plotly-create-a-scatter-with-categorical-x-axis-jitter-and-multi-level-axis
+ * @param  {json} data Data loaded in a previous step
+ * @param  {Array} name_vars Variable(s) to group by
+ * @return {Array} traces List of traces (data) as input to a plotly graph
+ */
+function processData(data, name_var) {
+    try {
+        traces = [];
+
+        // construct plotly "dataframe"
+        x = [];
+        names = [];
+        text = [];
+
+        data.forEach(function (val) {
+            if (val[name_var]) {
+                x.push(val['Date of Event']);
+                names.push(val[name_var]);
+                text.push(
+                    '<a href="' +
+                        val['Link'] +
+                        '" target="_blank">' +
+                        val['Source'].replaceAll('\n', '<br>') +
+                        '</a>' +
+                        '<br>' +
+                        val['Year']
+                );
+            }
+        });
+
+        traces.push({
+            type: 'box',
+            name: names,
+            x: x,
+            text: text,
+            boxpoints: 'all', // show points used in box plot
+            jitter: 1, // so points don't overlap
+            pointpos: 0, // center points
+            marker: {
+                size: 15,
+                opacity: 0.5,
+                cliponaxis: false // Allow points to extend beyond the axis boundaries
+            },
+            transforms: [
+                {
+                    type: 'groupby',
+                    groups: names,
+                    // assign color based on group (cite: GPT)
+                    styles: names.map((group, i) => ({
+                        target: group,
+                        value: {
+                            marker: { color: colorbook[name_var][group] }
+                        }
+                    }))
+                }
+            ],
+            fillcolor: 'rgba(0,0,0,0)', // remove box part of boxplot
+            line: { color: 'rgba(0,0,0,0)' }, // remove box part of boxplot
+            hoverinfo: 'text',
+            hovertemplate: '%{text}<extra></extra>' // the <extra> tag removes any excess formatting
+        });
+
+        // console.log(traces)
+        return traces;
+    } catch (error) {
+        console.error('Error processing data: ', error);
+    }
+}
+
+/**
+ * Waypoints (scroll interactions) for article body.
+ * Cite:
+ * @param  {str} Name of HTML div to attach waypoint to
+ * @param  {json} mapping Maps div name to the proper zoom ranges
+ */
+function createWaypoint(div, mapping, offset = '80%') {
+    function handler(direction) {
+        myPlot = document.getElementById('chart-div');
+
+        // https://stackoverflow.com/questions/9279368/how-to-get-all-css-classes-of-an-element
+        let classList = document.getElementById(div).className;
+
+        if (direction == 'down') {
+            // go to next key
+            Plotly.animate(
+                myPlot,
+                {
+                    layout: {
+                        yaxis: { range: mapping[div]['y'] }
+                    }
+                },
+                { transition: transition }
+            );
+            console.log(div);
+        } else if (div == 'palestine') {
+            Plotly.animate(
+                myPlot,
+                {
+                    layout: {
+                        yaxis: { range: mapping['all']['y'] }
+                    }
+                },
+                { transition: transition }
+            );
+            console.log('first key');
+        } else if (!classList.match('first')) {
+            // go back to the previous key
+
+            let orderedKeys = [];
+            for (var key in mapping) {
+                orderedKeys.push(key);
+            }
+
+            previousIndex = orderedKeys.indexOf(div) - 1;
+            previousKey = orderedKeys[previousIndex];
+
+            Plotly.animate(
+                myPlot,
+                {
+                    layout: {
+                        yaxis: { range: mapping[previousKey]['y'] }
+                    }
+                },
+                { transition: transition }
+            );
+            console.log('prev key');
+        }
+    }
+
+    new Waypoint({
+        element: document.getElementById(div),
+        handler: handler,
+        offset: offset
+    });
+}
+
+/**
+ * Waypoints (scroll interactions) for article body.
+ * Cite:
+ * @param  {str} Name of HTML div to attach waypoint to
+ * @param  {json} mapping Maps div name to the proper zoom ranges
+ */
+function createNewSection(div, variable, prev_var, mapping, offset = '80%') {
+    new Waypoint({
+        element: document.getElementById(div),
+        handler: function (direction) {
+            var myPlot = document.getElementById('chart-div');
+
+            if (direction == 'down') {
+                showChart();
+
+                Plotly.newPlot(
+                    'chart-div',
+                    processData(data, variable),
+                    layout,
+                    config
+                );
+
+                Plotly.animate(
+                    myPlot,
+                    {
+                        layout: {
+                            yaxis: { range: mapping[variable]['all']['y'] }
+                        }
+                    },
+                    { transition: transition }
+                );
+
+                console.log('New section created at: ' + div);
+            } else if (prev_var == 'Top') {
+                // if going back to intro, hide chart
+                hideChart();
+            } else {
+                // go to last key of previous section
+
+                let orderedKeys = [];
+                for (var key in mapping[prev_var]) {
+                    orderedKeys.push(key);
+                }
+
+                Plotly.newPlot(
+                    'chart-div',
+                    processData(data, prev_var),
+                    layout,
+                    config
+                );
+
+                Plotly.animate(
+                    myPlot,
+                    {
+                        layout: {
+                            yaxis: {
+                                range: mapping[prev_var][orderedKeys.at(-1)][
+                                    'y'
+                                ]
+                            }
+                        }
+                    },
+                    { transition: transition }
+                );
+
+                console.log('Going to previous section: ' + prev_var);
+            }
+
+            myPlot.on('plotly_click', open_url);
+            myPlot.on('plotly_hover', hide_box_hovers);
+        },
+        offset: offset
+    });
+}
+
+/**
+ * When you click on a data point, it should open the URL linked in the hover text in a new tab.
+ * Cite: GPT
+ * @param {json} data Data sensed from plotly hover event
+ */
+function open_url(data) {
+    var info = data.points[0];
+    var url = info.text.match(/href="(.*)" /);
+    // console.log(url[1])
+
+    window.open(url[1], '_blank').focus();
+}
+
+/**
+ * For plotly boxplots, hovering over the boxplot will show its summary statistics. Because I am using boxplots only for the built-in jitter function, keeping the points and hiding the box part, I need to disable the summary statistic hover.
+ * Cite: GPT
+ * @param {json} data Data sensed from plotly hover event
+ */
+function hide_box_hovers(data) {
+    hoverLayer = document.querySelector('.hoverlayer');
+
+    if (data.points.length == 7) {
+        hoverLayer.style.display = 'none';
+    } else {
+        hoverLayer.style.display = 'block';
+    }
+}
+
+// ------- CONSTANTS ------
+
+/**
+ * layout used for all plots. As opposed to the data object, this should contain parameters that are constant across the entire graph, not variable across traces or groups
+ * Cite: https://community.plotly.com/t/date-tick-formatting/11081/5
+ */
+const layout = {
+    title: {
+        text: 'Divestment Activism Events at UChicago',
+        x: 0.14,
+        font: {
+            size: 20
+        },
+        subtitle: {
+            text: 'Click a data point to visit the source article.',
+            font: {
+                size: 14
+            }
+        }
+    },
+    font: {
+        family: 'Georgia'
+    },
+    xaxis: {
+        showgrid: true,
+        showline: true,
+        range: ['1964-1-1', '2027-1-1'],
+        type: 'date',
+        dtick: 'M60',
+        ticklabelstep: 1,
+        tickfont: {
+            size: 14
+        }
+    },
+    yaxis: {
+        showgrid: false,
+        ticktext: 'text',
+        tickfont: {
+            size: 14
+        }
+    },
+    hovermode: 'closest',
+    hoverlabel: {
+        bgcolor: 'white'
+    },
+    showlegend: false,
+    margin: {
+        l: 120,
+        r: 15
+    }
+};
+
+const config = {
+    displayModeBar: false,
+    responsive: true
+};
+
+const transition = {
+    duration: 400,
+    easing: 'linear'
+};
+
+const colorbook = {
+    Movement: {
+        Palestine: 'rgb(128, 0, 0)',
+        // 'rgb(255, 163, 25)',
+        'Fossil fuels': 'rgb(193, 102, 34)',
+        'Uyghur rights': 'rgb(143, 57, 49)',
+        'Labor rights': 'rgb(138, 144, 69)',
+        SRIC: 'rgb(88, 89, 63)',
+        Sudan: 'rgb(21, 95, 131)',
+        'South Africa': 'rgb(53, 14, 32)'
+        // 'rgb(71, 181, 255)'
+        // 'rgb(255, 51, 153)'
+    },
+    'Type of Action': {
+        'Letter writing': 'rgb(128, 0, 0)',
+        Protest: 'rgb(193, 102, 34)',
+        Resolution: 'rgb(143, 57, 49)',
+        'Legal action': 'rgb(138, 144, 69)',
+        'Research report': 'rgb(88, 89, 63)',
+        'Public event': 'rgb(21, 95, 131)'
+    },
+    'Admin Response': {
+        Arrest: 'rgb(128, 0, 0)',
+        'Disciplinary action': 'rgb(88, 89, 63)',
+        Negotiation: 'rgb(193, 102, 34)',
+        'Public response': 'rgb(143, 57, 49)',
+        'Non-<br>divestment<br>support': 'rgb(138, 144, 69)'
+    },
+    Administration: {
+        Alivisatos: 'rgb(128, 0, 0)',
+        Zimmer: 'rgb(193, 102, 34)',
+        Randel: 'rgb(143, 57, 49)',
+        Gray: 'rgb(138, 144, 69)',
+        Wilson: 'rgb(88, 89, 63)',
+        Beadle: 'rgb(21, 95, 131)'
+    }
+};
+
+const zoom_mapping = {
+    Movement: {
+        all: { y: [-0.5, 6.5] },
+        palestine: { y: [-0.25, 0.25] },
+        'fossil-fuels': { y: [0.75, 1.25] },
+        'labor-rights': { y: [1.75, 2.25] },
+        'uyghur-rights': { y: [2.75, 3.25] },
+        sric: { y: [3.75, 4.25] },
+        sudan: { y: [4.75, 5.25] },
+        'south-africa': { y: [5.75, 6.25] }
+    },
+    'Type of Action': {
+        all: { y: [-0.5, 5.5] },
+        letters: { y: [0.75, 1.25] },
+        protest: { y: [-0.25, 0.25] },
+        'other-action': { y: [1.5, 5.5] }
+    },
+    'Admin Response': {
+        all: { y: [-0.5, 4.5] },
+        meeting: { y: [2.75, 4.25] },
+        police: { y: [0.75, 2.25] },
+        'other-response': { y: [-0.25, 0.25] }
+    }
+};
+
+// -------- MAIN --------
+var data;
+
+async function init() {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    };
+
+    data = await fetchData();
+    // console.log(data);
+
+    quoteWP = new Waypoint({
+        element: document.querySelector('#transition'),
+        handler: function (direction) {
+            if (direction == 'down') {
+                d3.selectAll('.attribution')
+                    .transition()
+                    .duration(1500)
+                    .style('opacity', 1);
+                // console.log('quotes');
+            }
+        },
+        offset: '100%'
+    });
+
+    quoteDoneWP = new Waypoint({
+        element: document.querySelector('#transition'),
+        handler: function (direction) {
+            if (direction == 'down') {
+                d3.selectAll('#pre-intro')
+                    .transition()
+                    .duration(500)
+                    .style('opacity', 0)
+                    .style('z-index', '-1');
+                console.log('hide quotes');
+            } else {
+                d3.selectAll('#pre-intro')
+                    .transition()
+                    .duration(0)
+                    .style('z-index', '100')
+                    .style('opacity', 100);
+                console.log('show quotes');
+            }
+        },
+        offset: '-10%'
+    });
+
+    // we will edit this plot throughout the whole article
+    createNewSection(
+        'causes',
+        'Movement',
+        (prev_var = 'Top'),
+        (mapping = zoom_mapping),
+        (offset = '110%')
+    );
+
+    // define waypoints (scroll reactions)
+    createWaypoint('palestine', zoom_mapping['Movement']);
+    createWaypoint('fossil-fuels', zoom_mapping['Movement']);
+    createWaypoint('uyghur-rights', zoom_mapping['Movement']);
+    createWaypoint('labor-rights', zoom_mapping['Movement']);
+    createWaypoint('sric', zoom_mapping['Movement']);
+    createWaypoint('sudan', zoom_mapping['Movement']);
+    createWaypoint('south-africa', zoom_mapping['Movement']);
+
+    createNewSection(
+        'actions',
+        'Type of Action',
+        (prev_var = 'Movement'),
+        (mapping = zoom_mapping)
+    );
+
+    createWaypoint('letters', zoom_mapping['Type of Action']);
+    createWaypoint('protest', zoom_mapping['Type of Action']);
+    createWaypoint('other-action', zoom_mapping['Type of Action']);
+
+    createNewSection(
+        'admin',
+        'Admin Response',
+        (prev_var = 'Type of Action'),
+        (mapping = zoom_mapping)
+    );
+
+    createWaypoint('meeting', zoom_mapping['Admin Response']);
+    createWaypoint('police', zoom_mapping['Admin Response']);
+    createWaypoint('other-response', zoom_mapping['Admin Response']);
+
+    finalWP = new Waypoint({
+        element: document.getElementById('conclusion'),
+        handler: function (direction) {
+            if (direction == 'down') {
+                hideChart();
+                console.log('Hiding chart div');
+            } else {
+                showChart();
+                console.log('Showing chart div');
+            }
+        },
+        offset: '90%'
+    });
+}
+
+// when page is loaded, define custom JS
+document.addEventListener('DOMContentLoaded', function () {
+    init();
+});
