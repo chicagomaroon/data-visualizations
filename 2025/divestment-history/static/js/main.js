fc7465f3ec4759b2ff3687176742debfe3b3dfae
Karen <52115905+notkarenyi@users.noreply.github.com>
2025-04-22 10:27:31 -0500

    Address edits from Grey (#153)

--- 2025/divestment-history/static/js/main.js
+++ 2025/divestment-history/static/js/main.js
@@ -112,6 +112,11 @@ function createWaypoint(div, mapping, offset = '80%') {
     function handler(direction) {
         myPlot = document.getElementById('chart-div');
 
+        Plotly.relayout(myPlot, {
+            shapes: [], // clear shapes from admin section
+            annotations: [] // clear notes from admin section
+        });
+
         // https://stackoverflow.com/questions/9279368/how-to-get-all-css-classes-of-an-element
         let classList = document.getElementById(div).className;
 
@@ -181,6 +186,45 @@ function createNewSection(div, variable, prev_var, mapping, offset = '80%') {
         handler: function (direction) {
             var myPlot = document.getElementById('chart-div');
 
+            shapes = [];
+            annotes = [];
+
+            if (variable == 'Admin Response') {
+                for (var president in history) {
+                    start = new Date(history[president]['x'][0]);
+                    end = new Date(history[president]['x'][1]);
+                    shapes.push({
+                        type: 'rect',
+                        x0: start,
+                        y0: -1,
+                        x1: end,
+                        y1: 7,
+                        line: {
+                            width: 1,
+                            color: '#aaaaaa'
+                        },
+                        fillcolor: 'rgba(0,0,0,0)'
+                        // layer: 'below',
+                        // opacity: .1
+                    });
+                    mid = new Date(
+                        start.getFullYear() +
+                            (end.getFullYear() - start.getFullYear()) / 2,
+                        6,
+                        1
+                    );
+                    annotes.push({
+                        x: mid,
+                        y: -0.5,
+                        text: president,
+                        showarrow: false,
+                        font: {
+                            color: '#8a8a8a'
+                        }
+                    });
+                }
+            }
+
             if (direction == 'down') {
                 showChart();
 
@@ -195,7 +239,9 @@ function createNewSection(div, variable, prev_var, mapping, offset = '80%') {
                     myPlot,
                     {
                         layout: {
-                            yaxis: { range: mapping[variable]['all']['y'] }
+                            yaxis: { range: mapping[variable]['all']['y'] },
+                            shapes: shapes,
+                            annotations: annotes
                         }
                     },
                     { transition: transition }
@@ -298,7 +344,7 @@ const layout = {
     xaxis: {
         showgrid: true,
         showline: true,
-        range: ['1964-1-1', '2027-1-1'],
+        range: ['1964-6-1', '2026-1-1'],
         type: 'date',
         dtick: 'M60',
         ticklabelstep: 1,
@@ -337,15 +383,12 @@ const transition = {
 const colorbook = {
     Movement: {
         Palestine: 'rgb(128, 0, 0)',
-        // 'rgb(255, 163, 25)',
         'Fossil fuels': 'rgb(193, 102, 34)',
         'Uyghur rights': 'rgb(143, 57, 49)',
         'Labor rights': 'rgb(138, 144, 69)',
         SRIC: 'rgb(88, 89, 63)',
         Sudan: 'rgb(21, 95, 131)',
         'South Africa': 'rgb(53, 14, 32)'
-        // 'rgb(71, 181, 255)'
-        // 'rgb(255, 51, 153)'
     },
     'Type of Action': {
         'Letter writing': 'rgb(128, 0, 0)',
@@ -376,10 +419,10 @@ const zoom_mapping = {
     Movement: {
         all: { y: [-0.5, 6.5] },
         palestine: { y: [-0.25, 0.25] },
-        'fossil-fuels': { y: [0.75, 1.25] },
-        'labor-rights': { y: [1.75, 2.25] },
-        'uyghur-rights': { y: [2.75, 3.25] },
-        sric: { y: [3.75, 4.25] },
+        'fossil-fuels': { y: [0.5, 1.5] },
+        'labor-rights': { y: [1.25, 2.75] },
+        'uyghur-rights': { y: [2.25, 3.75] },
+        sric: { y: [3.5, 4.5] },
         sudan: { y: [4.75, 5.25] },
         'south-africa': { y: [5.75, 6.25] }
     },
@@ -390,11 +433,22 @@ const zoom_mapping = {
         'other-action': { y: [1.5, 5.5] }
     },
     'Admin Response': {
-        all: { y: [-0.5, 4.5] },
-        meeting: { y: [2.75, 4.25] },
-        police: { y: [0.75, 2.25] },
-        'other-response': { y: [-0.25, 0.25] }
+        all: { y: [-0.75, 4.5] },
+        meeting: { y: [2.3, 4.75] },
+        police: { y: [0.3, 2.75] },
+        'other-response': { y: [-1, 0.75] }
     }
+};
+
+const history = {
+    Beadle: { x: ['1963-1-1', '1968-1-1'] },
+    Levi: { x: ['1968-1-1', '1975-1-1'] },
+    Wilson: { x: ['1975-1-1', '1978-8-1'] },
+    Gray: { x: ['1978-8-1', '1993-1-1'] },
+    Sonnenschein: { x: ['1993-1-1', '2000-1-1'] },
+    Randel: { x: ['2000-1-1', '2006-1-1'] },
+    Zimmer: { x: ['2006-1-1', '2021-1-1'] },
+    Alivisatos: { x: ['2021-1-1', '2026-6-1'] }
 };
 
 // -------- MAIN --------
@@ -419,7 +473,7 @@ async function init() {
                 // console.log('quotes');
             }
         },
-        offset: '100%'
+        offset: '90%'
     });
 
     quoteDoneWP = new Waypoint({
@@ -430,7 +484,7 @@ async function init() {
                 console.log('hide quotes');
             }
         },
-        offset: '30%'
+        offset: '70%'
     });
 
     // we will edit this plot throughout the whole article
@@ -439,7 +493,7 @@ async function init() {
         'Movement',
         (prev_var = 'Top'),
         (mapping = zoom_mapping),
-        (offset = '90%')
+        (offset = '70%')
     );
 
     // define waypoints (scroll reactions)
