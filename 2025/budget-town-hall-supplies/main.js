61f541a4a371fdcde96ece8296b22ad355417baf
Karen <notkarenyi@gmail.com>
2025-04-26 14:25:12 -0500

    replicate budget progress charts

---
+++ 2025/budget-town-hall-supplies/main.js
@@ -0,0 +1,85 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Georgia, serif'
+        },
+        type: 'column'
+    },
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#800000',
+        // '#FFA319',
+        // '#C16622',
+        // '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    title: {
+        text: 'Progress on FY25 Budget: Supplies, Services and Other Expenses',
+        align: 'center'
+    },
+
+    // subtitle: {
+    //     text: 'Administration plans to increase',
+    //     align: 'center',
+    //     style: {
+    //         fontSize: '15px'
+    //     }
+    // },
+
+    tooltip: {
+        valueDecimals: 0,
+        valuePrefix: '$',
+        valueSuffix: 'M'
+    },
+
+    accessibility: {
+        valuePrefix: '$',
+        valueSuffix: 'M'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    xAxis: {
+        categories: ['FY25 Final', 'FY25 Budget', 'FY25 Projected']
+    },
+
+    yAxis: {
+        labels: {
+            format: '${text}M',
+            style: {
+                fontSize: '16px'
+            }
+        },
+        title: {
+            enabled: false
+        },
+        tickPositions: [0, 200, 400, 600, 800, 1000, 1200]
+    },
+
+    series: [
+        {
+            name: 'Expenses',
+            data: [869, 1126, 1146],
+            dataLabels: {
+                enabled: true,
+                format: '${point.y}M'
+            }
+        }
+    ]
+});
---
+++ 2025/budget-town-hall-tuition/main.js
@@ -0,0 +1,85 @@
+// For API and chart documentation please look here:
+// https://www.highcharts.com/demo
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'Georgia, serif'
+        },
+        type: 'column'
+    },
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('chart-div', {
+    // Setting default colors
+    colors: [
+        '#800000',
+        // '#FFA319',
+        // '#C16622',
+        // '#8F3931',
+        '#8A9045',
+        '#58593F',
+        '#155F83',
+        '#350E20',
+        '#47B5FF',
+        '#FF3399'
+    ],
+
+    title: {
+        text: 'Progress on FY25 Budget: Net Tuition Revenue',
+        align: 'center'
+    },
+
+    // subtitle: {
+    //     text: 'Administration plans to increase',
+    //     align: 'center',
+    //     style: {
+    //         fontSize: '15px'
+    //     }
+    // },
+
+    tooltip: {
+        valueDecimals: 0,
+        valuePrefix: '$',
+        valueSuffix: 'M'
+    },
+
+    accessibility: {
+        valuePrefix: '$',
+        valueSuffix: 'M'
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    xAxis: {
+        categories: ['FY25 Final', 'FY25 Budget', 'FY25 Projected']
+    },
+
+    yAxis: {
+        labels: {
+            format: '${text}M',
+            style: {
+                fontSize: '16px'
+            }
+        },
+        title: {
+            enabled: false
+        },
+        tickPositions: [0, 200, 400, 600, 800]
+    },
+
+    series: [
+        {
+            name: 'Revenue',
+            data: [610, 696, 689],
+            dataLabels: {
+                enabled: true,
+                format: '${point.y}M'
+            }
+        }
+    ]
+});
