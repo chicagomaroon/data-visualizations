fc9e5df710dc745f8eb1e677dd9588603dc0fa47
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-05-14 22:19:23 -0500

    add pres

--- 2025/trustee-donations/trustee-pres/main.js
+++ 2025/trustee-donations/trustee-pres/main.js
@@ -8,40 +8,72 @@ Highcharts.setOptions({
     }
 });
 
+cycles = [
+    '1991-92',
+    '1995-96',
+    '1999-00',
+    '2003-04',
+    '2007-08',
+    '2011-12',
+    '2015-16',
+    '2019-20',
+    '2023-24'
+];
+
+demCount = [2, 1, 7, 5, 22, 11, 16, 14, 12];
+
+repCount = [3, 2, 5, 5, 9, 8, 6, 2, 8];
+
 Highcharts.chart('chart-div', {
     // Setting default colors
-    colors: [
-        '#800000',
-        '#FFA319',
-        '#C16622',
-        '#8F3931',
-        '#8A9045',
-        '#58593F',
-        '#155F83',
-        '#350E20',
-        '#47B5FF',
-        '#FF3399'
-    ],
-
+    colors: ['#155F83', '#800000'],
     // All code for your chart goes here
     title: {
-        text: '[TITLE]',
+        text: 'Number of Current Trustees Who Contributed to Presidential Candidates',
         align: 'center'
     },
+    subtitle: {
+        text: 'In recent presidential election cycles, more trustees have given to Democrats than Republicans.',
+        align: 'center'
+    },
+    chart: {
+        type: 'column'
+    },
 
-    plotOptions: {
-        series: {
-            // PUT PLOT CONFIG OPTIONS HERE SPACE FOR YOUR DATA
+    xAxis: {
+        categories: cycles,
+        title: {
+            text: ''
+        },
+
+        labels: {
+            rotation: -30,
+            style: {
+                fontSize: '12px',
+                textOverflow: 'none'
+            }
         }
+    },
+
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total Trustees'
+        }
+    },
+
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b><br/>'
     },
 
     series: [
         {
-            name: '[DATA FIELD NAME]',
-            label: {
-                enabled: false
-            },
-            data: []
+            name: 'Number of Trustees giving to Democrats',
+            data: demCount
+        },
+        {
+            name: 'Number of Trustees giving to Republicans',
+            data: repCount
         }
     ]
 });
