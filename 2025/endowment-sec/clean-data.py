5d82b553324e5a69042eebcb4ee03ad5ab533260
Karen <notkarenyi@gmail.com>
2025-09-01 00:06:26 -0500

    fix parsing sec

--- 2025/endowment-sec/clean-data.py
+++ 2025/endowment-sec/clean-data.py
@@ -57,52 +57,55 @@ for i,investment in enumerate(investments):
         df = pl.DataFrame(data.infotable)
         date = datetime.strptime(data.report_period, "%Y-%m-%d").date()
     except AttributeError as e:
-        print(f"Filing has no HTML")
+        print(f"Filing has no HTML, parsing text: {i}")
         data = investment.markdown()
         date = datetime.strptime(investment.report_date, "%Y-%m-%d").date()
 
         schema = [
-            ('Issuer', String),
-            ('Class', String),
-            ('Cusip', String),
-            ('Value', Int64),
-            ('SharesPrnAmount', String),
-            ('Type', String),
-            ('PutCall', String),
-            ('InvestmentDiscretion', String),
-            ('SoleVoting', Int64),
-            ('SharedVoting', Int64),
-            ('NonVoting', Int64),
-            ('Ticker', String),
-            ('Date', Date),
+            ("Issuer", String),
+            ("Class", String),
+            ("Cusip", String),
+            ("Value", Int64),
+            ("SharesPrnAmount", String),
+            ("Type", String),
+            ("PutCall", String),
+            ("InvestmentDiscretion", String),
+            ("SoleVoting", Int64),
+            ("SharedVoting", Int64),
+            ("NonVoting", Int64),
+            ("Ticker", String),
+            ("Date", Date),
         ]
 
-        if len(data)<2000:
+        if len(data) < 2000:
             print(f"WARNING: Filing has truncated data: {date}")
             continue
 
-        if '($1000)' in data: # eg sep 2007
+        if "($1000)" in data:  # eg sep 2007
             print(f"Using $1000: {date}")
             schema = [
-                ('Issuer', String),
-                ('Class', String),
-                ('Cusip', String),
-                ('Value', Int64),
-                ('SharesPrnAmount', String),
-                ('Type', String),
-                ('InvestmentDiscretion', String),
-                ('Ticker', String),
-                ('SoleVoting', Int64),
-                ('SharedVoting', Int64),
-                ('NonVoting', Int64),
+                ("Issuer", String),
+                ("Class", String),
+                ("Cusip", String),
+                ("Value", Int64),
+                ("SharesPrnAmount", String),
+                ("Type", String),
+                ("InvestmentDiscretion", String),
+                ("Ticker", String),
+                ("SoleVoting", Int64),
+                ("SharedVoting", Int64),
+                ("NonVoting", Int64),
             ]
-            lines = data.split('SHARED NONE')[-1].split('Other Managers')[0].splitlines()
+            # remove header and footer
+            lines = (
+                data.split("SHARED NONE")[-1].split("Other Managers")[0].splitlines()
+            )
             # remove line numbers
-            lines = [re.sub('^\\d+ *', '', row) for row in lines]
+            lines = [re.sub("^\\d+ *", "", row) for row in lines]
             rows = []
             for line in lines:
                 if len(line.strip()):
-                    line = re.sub(r'\s+', ' ', line)
+                    line = re.sub(r"\s+", " ", line)
                     cell = []
 
                     # split line into cells from right to left
@@ -111,94 +114,99 @@ for i,investment in enumerate(investments):
                         "(\\d{1,6})",
                         "(\\d{1,6})",
                         "(\\d{1,2})",
-                        "([OtherSole]{4,6})",
+                        "([OtherSoleOTHERSOLE]{4,6})",
                         "([SH]{2})",
                         "(\\d{2,7})",
                         "(\\d{1,7})",
                         "([A-Z\\d]{8,9})",
-                        "((A)|([AB]* *[COM|BEN|ADR]+))",
                     ]
 
                     for pattern in patterns:
-                        split = line.rsplit(' ', 1)
+                        split = line.rsplit(" ", 1)
                         if re.search(pattern, split[-1]):
                             cell.append(split[-1])
                             line = split[0]
                         else:
                             cell.append(None)
-                    cell.append(line.strip()) # rest of the line is issuer
+                    try:
+                        cls = re.search(
+                            "(NLGO 03|CV C|Non Cum Perp|Ishares Mid Val (FD)?|SUB Vtg|(Cl |CL )?(A )?(B )?(Com|COM|BEN|ADR|CAP|PFD|PLC|RTS|REIT|INT)( Biosurgery| Molec Onc)?( EXCH)?|A|B)$",
+                            line.strip(),
+                        ).group()
+                    except Exception as e:
+                        print(f"Error parsing class: {line} - {e}")
+                        cls = None
+                    cell.append(cls)
+                    cell.append(line.strip(cls).strip())  # rest of the line is issuer
                     rows.append(cell[::-1])
-        elif '----' in data: # eg 1999
+        elif "----" in data:  # eg 1999
             print(f"Using --------- : {date}")
-            
+
             schema = [
-                ('Issuer', String),
-                ('Class', String),
-                ('Cusip', String),
-                ('Value', Int64),
-                ('SharesPrnAmount', String),
-                ('Type', String),
-                ('InvestmentDiscretion', String),
-                ('Ticker', String),
-                ('SoleVoting', Int64),
-                ('SharedVoting', Int64),
-                ('NonVoting', Int64),
+                ("Issuer", String),
+                ("Class", String),
+                ("Cusip", String),
+                ("Value", Int64),
+                ("SharesPrnAmount", String),
+                ("Type", String),
+                ("InvestmentDiscretion", String),
+                ("Ticker", String),
+                ("SoleVoting", Int64),
+                ("SharedVoting", Int64),
+                ("NonVoting", Int64),
             ]
-            lines = re.split('-+\\s*\\n',data)[-1].splitlines()
-            lines = [re.sub('^\\d+ *', '', row) for row in lines]
+            lines = re.split("-+\\s*\\n", data)[-1].splitlines()
+            lines = [re.sub("^\\d+ *", "", row) for row in lines]
             # cite: copilot
-            if len(lines[2].rsplit(' ',10))<10:
+            if len(lines[2].rsplit(" ", 10)) < 10:
                 rows = [cells[i] + cells[i + 1] for i in range(0, len(cells), 2)]
             cells = []
             for line in lines:
-                if len(line.strip()) and 'TABLE' not in line:
-                    line = re.sub(r'\s+', ' ', line)
-                    cell = line.rsplit(' ',10)
+                if len(line.strip()) and "TABLE" not in line:
+                    line = re.sub(r"\s+", " ", line)
+                    cell = line.rsplit(" ", 10)
                     cells.append(cell)
             rows = rows[:-1]  # remove last row since it is a summary row
-        else: # eg sep 2008
+        else:  # eg sep 2008
             print(f"Using missing case: {date}")
-            lines = data.split('SHARED NONE')[-1].splitlines()
-            lines = [re.sub('^\\d+ *', '', row) for row in lines]
-            cells = [re.split(' +',line.strip('</pre>')) for line in lines if len(line.strip()) and not re.search('</.+>',line)]
+            lines = data.split("SHARED NONE")[-1].splitlines()
+            lines = [re.sub("^\\d+ *", "", row) for row in lines]
+            cells = [
+                re.split(" +", line.strip("</pre>"))
+                for line in lines
+                if len(line.strip()) and not re.search("</.+>", line)
+            ]
             # cite: copilot
             rows = rows[:-3]  # remove last rows
 
         try:
-            
             print(rows[0])
-            df = pl.DataFrame(
-                rows,
-                schema=schema
-            )
+            df = pl.DataFrame(rows, schema=schema)
         except ShapeError as e:
             # print(data[:1000])
             print(rows)
             print(schema)
-            print([(i,len(row)) for i,row in enumerate(rows)])
+            print([(i, len(row)) for i, row in enumerate(rows)])
             print(f"{i} {date} filing has shape error: {e}")
             raise e
         except ComputeError as e:
             print(rows)
             print(f"{i} {date} filing has row mismatch: {e}")
             raise e
-        
-    # if 2021 or later, divide value by 1000
-    # cover pages state that reports past this date are reported in dollars instead of thousands 
+
+    # if 2021 or later, divide value by 1000 (we will report all data in thousands)
+    # cover pages state that reports past this date are reported in dollars instead of thousands
+    # note that official SEC documentation lists data format change date as 1/3/2023 but I do not see this reflected in the data https://www.sec.gov/files/form_13f_readme.pdf
     # https://www.sec.gov/Archives/edgar/data/314957/000110465924034451/xslForm13F_X02/primary_doc.xml
     if date >= datetime(2020, 12, 1).date():
-        df = df.with_columns(
-            (pl.col('Value') / 1000).cast(Int64).alias('Value')
-        )
+        df = df.with_columns((pl.col("Value") / 1000).cast(Int64).alias("Value"))
 
     # add date column
-    df = df.with_columns(
-        pl.lit(date).alias("Date")
-    )
+    df = df.with_columns(pl.lit(date).alias("Date"))
 
     # print(df)
 
-    sec = pl.concat([sec, df],how="diagonal")
+    sec = pl.concat([sec, df], how="diagonal")
 
 sec.write_csv("13F-HR.csv")
 
