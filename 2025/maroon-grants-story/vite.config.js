5c4ee7da2f145647b3e4e86e97f232356f6e107b
Nik4002 <pateln4002@gmail.com>
2025-10-08 16:25:09 -0500

    Run formatter

--- 2025/maroon-grants-story/vite.config.js
+++ 2025/maroon-grants-story/vite.config.js
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/maroon-grants-story/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    base: '/maroon-grants-story/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html')
+            }
+        }
     },
-  },
-}); 
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
+});
