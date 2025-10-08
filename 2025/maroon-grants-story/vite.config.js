76265b2c5e82991a3d1533c237357f77c23b5590
Nolan Shaffer <nshaff3r@gmail.com>
2025-10-08 17:33:39 -0500

    deployment fixes

--- 2025/maroon-grants-story/vite.config.js
+++ 2025/maroon-grants-story/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/maroon-grants-story/',
+  base: '/2025/maroon-grants-story/',
   build: {
     rollupOptions: {
       input: {
