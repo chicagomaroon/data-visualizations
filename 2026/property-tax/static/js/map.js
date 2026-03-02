38af31d82e9dd2894d35301ee0023b4d0d7bda6d
Jinny Kim <jinny.kim1208@gmail.com>
2026-03-01 21:02:14 -0600

    Modify map.js

--- 2026/property-tax/static/js/map.js
+++ 2026/property-tax/static/js/map.js
@@ -8,6 +8,13 @@ var map = new maplibregl.Map({
     zoom: 13,
     interactive: false
 });
+    var map = new maplibregl.Map({
+        container: 'map',  
+        style: 'https://tiles.openfreemap.org/styles/positron',
+        center: [-87.6086, 41.7942],  
+        zoom: 13,
+        interactive: false
+    });
 
 // -------------------------------------------------------
 // load data for the main map
@@ -29,70 +36,66 @@ Promise.all([
             data: exemptData
         });
 
-        // all properties
-        map.addLayer({
-            id: 'property-parcels',
-            type: 'fill',
-            source: 'properties',
-            paint: {
-                'fill-color': [
-                    'match',
-                    ['get', 'mailing_name'],
-                    'University of Chicago',
-                    '#A52519',
-                    'DePaul University',
-                    '#003DA5',
-                    'Loyola University',
-                    '#BE9112',
-                    'Northwestern University',
-                    '#833FE8',
-                    'University of Illinois',
-                    '#FF5F05',
-                    '#99999901'
-                ],
-                'fill-opacity': 0
-            }
-        });
+            // all properties
+            map.addLayer({
+                id: "property-parcels", 
+                type: "fill",
+                source: "properties", 
+                paint: {
+                    "fill-color": [
+                        "match", 
+                        ["get", "mailing_name"],
+                            'University of Chicago', "#A52519",  
+                            'DePaul University', '#003DA5',
+                            'Loyola University', "#BE9112",   
+                            'Northwestern University', "#833FE8",
+                            'University of Illinois', '#FF5F05',
+                                                    '#99999901'
+                    ], 
+                    "fill-opacity": 0
+                }
+            });
 
-        // tax-exempt properties
-        map.addLayer({
-            id: 'exempt-parcels',
-            type: 'fill',
-            source: 'tax-exempt',
-            paint: {
-                'fill-color': [
-                    'match',
-                    ['get', 'owner_name'],
-                    'University of Chicago',
-                    '#A52519',
-                    '#99999901'
-                ],
-                'fill-opacity': 0
-            }
-        });
+            // tax-exempt properties
+            map.addLayer({
+                id: "exempt-parcels",
+                type: "fill",
+                source: "tax-exempt",
+                paint: {
+                    "fill-color": [
+                        "match",
+                        ["get","owner_name"],
+                        "University of Chicago","#A52519",
+                        "#99999901"
+                    ],
+                    "fill-opacity": 0
+                }
+             });
 
-        // highlight tax-exempt properties
-        map.addLayer({
-            id: 'property-highlight',
-            type: 'fill',
-            source: 'tax-exempt',
-            paint: {
-                'fill-color': '#A52519',
-                'fill-opacity': 0.9
-            },
-            filter: ['==', 'Name', '']
-        });
+            // highlight tax-exempt properties
+            map.addLayer({
+                id: "property-highlight",
+                type: "fill", 
+                source: "tax-exempt", 
+                paint: {
+                    "fill-color": "#A52519",
+                    "fill-opacity": 0.9,
+                },
+                filter: ["==", "Name", ""]
+            });
 
-        // highlight non-exempt properties
-        map.addLayer({
-            id: 'property-highlight-nonexempt',
-            type: 'fill',
-            source: 'properties',
-            paint: {
-                'fill-color': '#A52519',
-                'fill-opacity': 0.9
-            },
-            filter: ['==', 'Name', '']
-        });
+            // highlight non-exempt properties
+            map.addLayer({
+                id: "property-highlight-nonexempt",
+                type: "fill", 
+                source: "properties", 
+                paint: {
+                    "fill-color": "#A52519",
+                    "fill-opacity": 0.9,
+                },
+                filter: ["==", "Name", ""]
+            });
+
+    })
     });
-});
+
