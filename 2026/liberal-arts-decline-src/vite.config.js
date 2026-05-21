ccee70146851aa362549c80fb601f370e60986d5
Nolan Shaffer <nolanshaffer@uchicago.edu>
2026-05-21 02:59:01 -0500

    Majors piece (#218)
    <!--- 
    This Pull Request should contain all visuals related to one story.
    
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Create visuals for _____ story", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here: 
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## What does this PR do?
    
    <!---
    If this PR is for data visualizations you made, please follow these
    steps. Otherwise, please delete all of the text below.
    -->
    ## How was the functionality tested and verified?
    All steps should be completed in the order presented, and you should not
    move on to the next step until the previous is completed.
    - [ ] The title of each visual's `html` page is the same as the chart
    title.
    - [ ] The `meta_data.json` file for each visual is filled in correctly
    (the description is a complete sentence, and the title is the same as
    the visual).
    - [ ] Each visual and associated files has been minified using the `npm
    run process-visual` command.
    - [ ] Each visual was styled according to style guidelines (centered
    title/subtitle, uses our color scheme, etc.).
    - [ ] Each visual was approved by the lead developer.
    - [ ] Each visual was approved by the story's authors and/or
    stakeholders.
    - [ ] Each visual was submitted to Copy, and the resulting
    comments/edits have been resolved.
    
    <!-- Replace this comment with a screenshot of your visual. -->

--- 2026/liberal-arts-decline-src/vite.config.js
+++ 2026/liberal-arts-decline-src/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/liberal-arts-decline/'
+    base: '/data-visualizations/2026/liberal-arts-decline/'
 });
